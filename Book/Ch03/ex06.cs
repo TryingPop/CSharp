b1abedcbf8fc3eec58aba7b244a0f328a772cc1b
TryingPop <qotjdgns2001@gmail.com>
2022-07-13 17:24:33 +0900

    2022.07 Unity

---
+++ Book/Ch03/ex06.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 날짜 : 2022.07.13
+ * 내용 : 코드 3-6
+ * 
+ * 오전과 오후 구분하기
+ */
+
+namespace Book.Ch03
+{
+    internal class ex06
+    {
+        static void Main6(string[] args)
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("지금은 오전입니다.");
+            }
+            else
+            {
+                Console.WriteLine("지금은 오후입니다.");
+            }
+        }
+    }
+}
---
+++ Book/Ch03/ex13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 날짜 : 2022.07.13
+ * 내용 : 코드 3-13
+ * 
+ * 삼항 연산자
+ */
+
+namespace Book.Ch03
+{
+    internal class ex13
+    {
+        static void Main13(string[] args)
+        {
+            // 참과 거짓 위치에 불 자료형 사용
+            Console.Write("숫자 입력 : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine((number % 2 == 0) ? true : false);
+
+            // 참과 거짓 위치에 문자열 자료형 사용
+            Console.WriteLine((number % 2 == 0) ? "짝수" : "홀수");
+        }
+    }
+}

f412c876d810c4eaa42980425fca1cab84053ebf
TryingPop <qotjdgns2001@gmail.com>
2023-06-24 19:47:15 +0900

    fix(chat) : 2023. 6 Study 귓속말, 이름 변경 기능 추가
    앞으로 채팅내용 같은 것을 SQL에 저장하고,
    문자열 전송이 아닌 패킷을 설정해 송수신하기 기능이 필요하다
    
    문자열 보간에서 {0:D5}에서 {0 : D5}로 사용하면 에러뜨니 띄어쓰기 주의하자!

--- Private/26_Chat_Serv.cs
+++ Private/26_Chat_Serv.cs
@@ -8,17 +8,23 @@ using System.Net.Sockets;
 using System.Threading;
 
 /*
-날짜 : 2023. 6. 22
+날짜 : 2023. 6. 24
 이름 : 배성훈
 내용 : 채팅 서버
     멀티 쓰레드
     tcp/ip 통신
 
-    닉네임을 딕셔너리로 저장한다
-    클라쪽에서 연결과 동시에 이름을 같이 전송하니 데이터 수신이 안된다
+    귓속말 기능 추가
+    이름 바꾸기 기능 추가
 
-    컨텍스트 스위칭으로 데이터 손실인지 아니면 네트워크 전송에 따른 데이터 손실인지 모르겠다
-    일단은 콘솔에서 보기 좋게 만들기 위해 콘솔창 지우는 쪽부터 연습했다
+    귓속말을 읽는 방법 때문에 이름은 띄어쓰기가 불가능하게 했다
+    또한 이전 코드는 StreamWriter를 SendMessage 함수에서 매번 생성하기에
+    메모리 낭비가 심한거 같아 클래스를 생성하고 클래스를 이용하는 형식으로 바꿨다
+
+    그리고 귓말, 이름바꾸기 등을 표현하기 위해서 콘솔창에 색상 바꾸는 것을 추가했다
+
+    앞으로 추가할껀, 유저의 행동을 콘솔창에 보여주는 것 뿐만 아니라 데이터로 저장하는 것과
+    송수신 되는 데이터를 문자열이 아닌 구조체나 클래스로 바꿀 것이다.
 */
 
 namespace Private
@@ -29,131 +35,388 @@ namespace Private
         public static object key = new object();   // 임계영역용 오브젝트
 
         // public static List<TcpClient> clients = new List<TcpClient>();
-        public static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+        public static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();  // 이름과 클라이언트 소켓을 저장하는 딕셔너리
 
+        public static TcpListener tcpListener;
 
-        public static void Chat(object client)
+        public class Clnt
         {
 
-            TcpClient tcpClient = (TcpClient)client;
-            NetworkStream ns = tcpClient.GetStream();
-            StreamReader sr = new StreamReader(ns);
+            public static Dictionary<string, Clnt> clients = new Dictionary<string, Clnt>();
+
+            TcpClient client;
+            NetworkStream ns;
+            StreamReader sr;
+            StreamWriter sw;
 
             string msg = string.Empty;
-            string name = SetName();
+            char color = 'w';
+            public string name = string.Empty;
 
-            clients[name] = tcpClient;
-
-
-            try
+            public Clnt(TcpClient client)
             {
+
+                try
+                {
+                    this.client = client;
+                    this.ns = this.client.GetStream();
+                    this.sr = new StreamReader(this.ns);
+                    this.sw = new StreamWriter(this.ns);
+
+                    this.SetName();
+
+                    this.color = 'r';
+                    this.msg = "[소켓 연결]";
+                    PrintChat();
+                }
+                catch
+                {
+
+                    this.OnDisconnect();
+                }
+            }
+
+            /// <summary>
+            /// 이 소켓으로 메세지 문자열 전송
+            /// </summary>
+            /// <param name="message">보낼 메세지</param>
+            /// <param name="option">글자 색상</parma>
+            public void SendMessage(string message = null, int option = 0)
+            {
+
+                try
+                {
+
+                    if (option == 1)    // yellow
+                    {
+
+                        message = string.Format("cy|{0}", message);
+                    }
+                    else if (option == 2)   // red
+                    {
+
+                        message = string.Format("cr|{0}", message);
+                    }
+                    else if (option == 3)   // blue
+                    {
+
+                        message = string.Format("cb|{0}", message);
+                    }
+                    else if (option == 4)   // green
+                    {
+
+                        message = string.Format("cg|{0}", message);
+                    }
+
+
+                    sw.WriteLine(message);
+                    sw.Flush();
+                }
+                catch
+                {
+
+                    this.color = 'y';
+                    this.msg = string.Format("{0}에게 메세지 전송 실패", this.name);
+                    sw.Flush();
+                }
+            }
+
+            /// <summary>
+            /// 해당 소켓으로 "이름 : 메세지"로 전송할 때 사용하는 메소드, 즉 채팅 내용 전달 메소드 
+            /// </summary>
+            /// <param name="name">보낸 사람 이름</param>
+            /// <param name="message">보낼 메세지</param>
+            /// <param name="option">글자 색상</param>
+            public void SendMessage(string name, string message, int option = 0)
+            {
+
+                try
+                {
+
+                    if (option == 1)
+                    {
+
+                        name = string.Format("cy|{0}", name);
+                    }
+                    else if (option == 2)
+                    {
+
+                        name = string.Format("cr|{0}", name);
+                    }
+                    else if (option == 3)
+                    {
+
+                        name = string.Format("cb|{0}", name);
+                    }
+                    else if (option == 4)
+                    {
+
+                        name = string.Format("cb|{0}", name);
+                    }
+
+                    sw.WriteLine(string.Format("{0} : {1}", name, message));
+                    sw.Flush();
+                }
+                catch
+                {
+
+                    this.color = 'y';
+                    this.msg = string.Format("{0}에게 메세지 전송 실패", this.name);
+                    sw.Flush();
+                }
+            }
+
+            /// <summary>
+            /// 모든 유저에게 메세지 전달, 자기자신은 녹색 글로 전달한다
+            /// </summary>
+            /// <param name="option"></param>
+            public void SendAllUserMessage(int option = 0)
+            {
+
+                foreach (var data in Clnt.clients)
+                {
+
+                    if (data.Key != this.name)
+                    {
+
+                        data.Value.SendMessage(this.name, this.msg);
+                    }
+                    else
+                    {
+
+                        data.Value.SendMessage(this.msg, 4); // 자기자신은 녹색 칼라로 전달
+                    }
+                }
+            }
+
+            /// <summary>
+            /// 해당 소켓으로 부터 전송 받은 메세지, msg에 저장
+            /// </summary>
+            /// <returns>메세지를 전송 받았는지 여부</returns>
+            public bool RecvMessage()
+            {
+
+                this.msg = sr.ReadLine();
+
+                if (this.msg == null || this.msg == string.Empty)
+                {
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            /// <summary>
+            /// 소켓 해제, 즉 이 클래스의 참조 카운터를 0으로 만듦
+            /// </summary>
+            public void OnDisconnect()
+            {
+
+                try
+                {
+
+                    Clnt.clients.Remove(this.name);
+                }
+                catch { }
+
+                try
+                {
+
+                    this.sw?.Close();
+                }
+                catch { }
+
+                try
+                {
+
+                    this.sr?.Close();
+                }
+                catch { }
+
+                try
+                {
+
+                    this.ns?.Close();
+                }
+                catch { }
+
+                try
+                {
+
+                    this.client?.Close();
+                }
+                catch { }
+
+                this.msg = string.Format("[소켓 해제]");
+                this.color = 'r';
+                this.PrintChat();
+            }
+
+            /// <summary>
+            /// 특수 명령어 확인 및 행동 기록 메소드
+            /// 서버가 아닌 클라에서 명령어 구분을 위한 '|' 입력 시 전송 실패되게 했다
+            /// </summary>
+            /// <returns>메세지 전송이 필요한가 여부</returns>
+            public bool ChkCommand()
+            {
+
+                string chk = msg.Split('|')[0]; // 앞에서 null 체크를 했고 클라에서 '|' 문자 체크하므로 try - catch 구문 제외
+                if (chk == this.msg) { }
+
+                // 닉네임 변경
+                else if (chk == "sn")
+                {
+
+                    string beforename = this.name;
+
+                    this.SetName(msg.Substring(3));
+
+                    this.SendMessage(string.Format("{0}로 닉네임이 변경되었습니다.", this.name), 4);
+
+                    this.msg = $"[변경]{beforename}";
+                    this.color = '4';
+                    return false;
+                }
+                else if (chk == "w")        // 귓속말 기능
+                {
+
+                    if (clients.ContainsKey(msg.Split('|')[1]))
+                    {
+
+                        clients[msg.Split('|')[1]].SendMessage(this.name, msg.Split('|')[2], 3);
+
+                        this.msg = string.Format("[성공]{0} -> {1}", msg.Split('|')[1], msg.Split('|')[2]);
+                        this.color = 'b';
+                    }
+                    else
+                    {
+
+                        this.SendMessage("존재하지 않는 닉네임입니다.", 1);
+                        this.msg = string.Format("[실패]{0} -> {1}", msg.Split('|')[1], msg.Split('|')[2]);
+                        this.color = 'b';
+                    }
+
+                    return false;
+                }
+
+                this.msg = string.Format("{0}", this.msg);
+                this.color = 'w';
+                return true;
+            }
+
+            /// <summary>
+            /// 유저 행동 콘솔에 기록하기 위한 메소드
+            /// </summary>
+            public void PrintChat()
+            {
+
+                if (this.msg == null || this.msg == string.Empty)
+                {
+
+                    return;
+                }
+
+                this.SetColor();
+                Console.WriteLine("{0} : {1}", this.name, this.msg);
+                this.SetColor();
+
+                this.msg = string.Empty;
+            }
+
+            /// <summary>
+            /// 콘솔 칼라 적용 메소드
+            /// </summary>
+            private void SetColor()
+            {
+
+                if (this.color == 'r')
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (this.color == 'y')
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (this.color == 'b')
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (this.color == 'g')
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+
+                    Console.ResetColor();
+                }
+
+                this.color = 'w';
+            }
+
+            /// <summary>
+            /// 이름 설정 메소드
+            /// </summary>
+            /// <param name="name">바꿀 이름</param>
+            public void SetName(string name = null)
+            {
+
+                bool setRandName = (name == null || name == string.Empty) ? true : false;
+
+                try
+                {
+
+                    Clnt.clients.Remove(this.name); // 초기 생성인 경우 에러 발생한다 
+                }
+                catch { }
+
                 while (true)
                 {
 
-
-                    msg = sr.ReadLine();
-
-                    if (msg.Length > 50)
+                    if (setRandName)
                     {
 
-                        msg.Substring(0, 50);
+                        name = Clnt.GetRandomName();
                     }
 
-                    if (msg.Length != 0)
+                    if (Clnt.clients.ContainsKey(name))
                     {
 
-                        Monitor.Enter(key);
-
-                        foreach (var item in clients)
-                        {
-
-                            /*
-                            // 자기자신은 제외하는 코드
-                            if (item == tcpClient)
-                            {
-
-                                continue;
-                            }
-                            */
-
-                            SendMessage(item, msg);
-                        }
-                        Monitor.Exit(key);
-
-                        Console.WriteLine("메세지 : {0}\n전송완료", msg);
+                        setRandName = true;
+                        continue;
                     }
+
+                    this.name = name;
+
+                    if (this.name.Length >= 10)
+                    {
+
+                        this.name = this.name.Substring(0, 9);
+                        SendMessage("닉네임은 최대 9글자까지만 가능합니다.", 1);
+                    }
+
+                    Clnt.clients[this.name] = this;
+                    return;
                 }
             }
-            catch
+
+            /// <summary>
+            /// User + 01001 ~ 65536 사이의 임의의 숫자를 합친 닉네임 설정
+            /// </summary>
+            /// <returns>User01001 ~ User65535</returns>
+            private static string GetRandomName()
             {
 
-                Console.WriteLine("전송 에러");
+                Random rand = new Random();
+                return string.Format("User{0:D5}", rand.Next(1001, 65536));
             }
-            finally
-            {
 
-                sr.Close();
-
-                ns.Close();
-
-                clients.Remove(name);
-                tcpClient.Close();
-                Console.WriteLine("소켓 종료");
-            }
         }
-
-        public static string SetName(string name = null)
-        {
-
-            bool setRandName = (name == null || name == string.Empty) ? true : false;
-
-
-            while (true)
-            {
-
-                if (setRandName)
-                {
-
-                    name = SetRandomName();
-                }
-
-                if (clients.ContainsKey(name))
-                {
-
-                    setRandName = true;
-                    continue;
-                }
-
-                return name;
-            }
-        }
-
-        public static string SetRandomName()
-        {
-
-            Random rand = new Random();
-            return string.Format("User{0, 5}", rand.Next(1001, 65535));
-        }
-
-        private static void SendMessage(KeyValuePair<string, TcpClient> item, string msg)
-        {
-
-            NetworkStream ns = item.Value.GetStream();
-            StreamWriter sw = new StreamWriter(ns);
-
-            try
-            {
-
-                sw.WriteLine(string.Format("{0} : {1}", item.Key, msg));
-                sw.Flush();
-            }
-            catch
-            {
-
-                Console.WriteLine("전송 실패");
-            }
-        }
-
 
         /// <summary>
         /// 현재 서버의 아이피 확인
@@ -208,35 +471,77 @@ namespace Private
             }
         }
 
-        public static void ServStart()
+        /// <summary>
+        /// 서버 시작
+        /// </summary>
+        public static void StartServ()
         {
 
-            IPAddress ip;
-            int port = 0;
+            IPAddress ip = GetIP();
+            int port = SetPort();
 
-            ip = GetIP();
-            port = SetPort();
-            TcpListener tcpListener = new TcpListener(ip, port);
-
+            tcpListener = new TcpListener(ip, port);
+            tcpListener.Start();
 
             Console.Clear();
-            Console.WriteLine("IP : {0}", ip.ToString());
-            Console.WriteLine("Port : {0}", port.ToString());
-
-            tcpListener.Start();
-            Console.WriteLine("서버 시작");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[서버 시작]");
+            Console.WriteLine("IP : {0}\nPort : {1}", ip.ToString(), port.ToString());
+            Console.ResetColor();
 
             while (true)
             {
 
                 TcpClient tcpClient = tcpListener.AcceptTcpClient();
                 Monitor.Enter(key);
-                Console.WriteLine("클라이언트 접속 중...");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("클라이언트 접속 시도...");
+                Console.ResetColor();
+                Clnt clnt = new Clnt(tcpClient);
 
-                Thread thread = new Thread(new ParameterizedThreadStart(Chat));
-                thread.Start(tcpClient);
-                Console.WriteLine("클라이언트 접속 완료");
+                Thread thread = new Thread(new ParameterizedThreadStart(StartChat));
+                thread.Start(clnt);
+
                 Monitor.Exit(key);
+            }
+        }
+
+        /// <summary>
+        /// 해당 소켓의 채팅 시작
+        /// </summary>
+        /// <param name="client">채팅 시작</param>
+        public static void StartChat(object client)
+        {
+
+            Clnt clnt = (Clnt)client;
+            Clnt.clients[clnt.name] = clnt;
+
+            try
+            {
+
+                while (true)
+                {
+
+                    if (clnt.RecvMessage())
+                    {
+
+                        Monitor.Enter(key);
+                        if (clnt.ChkCommand())
+                        {
+
+                            clnt.SendAllUserMessage();
+                        }
+
+                        clnt.PrintChat();
+                        Monitor.Exit(key);
+                    }
+
+                }
+            }
+            catch
+            {
+
+                clnt.OnDisconnect();
             }
 
         }
@@ -244,7 +549,7 @@ namespace Private
         static void Main(string[] args)
         {
 
-            ServStart();
+            StartServ();
         }
     }
 }
--- Private/27_Chat_Clnt.cs
+++ Private/27_Chat_Clnt.cs
@@ -7,13 +7,11 @@ using System.Net;
 using System.Net.Sockets;
 
 /*
-날짜 : 2023. 6. 23
+날짜 : 2023. 6. 24
 이름 : 배성훈
 내용 : 채팅 클라
     멀티쓰레드
     tcp/ip 통신
- 
-    현재 닉네임 설정을 클라에서 정하고 전송하는게 아닌 서버에서 정해준 닉네임을 이용한다
 */
 
 namespace Private
@@ -21,35 +19,79 @@ namespace Private
     internal class _27_Chat_Clnt
     {
 
-        public static string msg = string.Empty;
+        public static string msg = string.Empty;        // 전송할 메세지
+        public static string name = string.Empty;       // 닉네임
 
-        public static TcpClient tcpClient;
+        public static TcpClient tcpClient;              // 클라이언트
+
 
         static void Main(string[] args)
         {
 
+            // 접속할 ip, port 
             string ip = "192.168.35.213";
             int port = 3303;
 
+            NetworkStream ns;
+            StreamWriter sw;
 
-            tcpClient = new TcpClient(ip, port);
-
-            NetworkStream ns = tcpClient.GetStream();
-            StreamWriter sw = new StreamWriter(ns);
-
-            Thread thread = new Thread(new ParameterizedThreadStart(RecvMessage));
-            thread.Start(tcpClient);
+            Thread thread;          // 수신용 쓰레드
 
 
             try
             {
+
+                Console.WriteLine("서버와 연결 시도... ");
+                tcpClient = new TcpClient(ip, port);    // 클라이언트 연결 시도
+
+                ns = tcpClient.GetStream();
+                sw = new StreamWriter(ns);
+
+                // 수신용 쓰레드
+                thread = new Thread(new ParameterizedThreadStart(RecvMessage));
+                thread.Start(tcpClient);
+
+                Console.WriteLine("서버와 연결되었습니다.");
+            }
+            catch   // 실패한 경우 종료
+            {
+
+                Console.WriteLine("서버와 연결이 실패했습니다.");
+                return;
+            }
+
+            try
+            {
+
+                // 전송을 목적으로 만들어진 코드
                 while (true)
                 {
 
+                    // 전송할 메세지 읽기
                     msg = Console.ReadLine();
 
+                    // 48자 넘어가면 48자로 짜른다
+                    // 서버에서 할려고 했으나 클라에게 부담을 주는게 맞다고 생각해 클라로 옮겼다
+                    if (msg.Length >= 48)
+                    {
+
+                        msg = msg.Substring(0, 48);
+                        Console.WriteLine("대화는 48자 이상 넘어갈 수 없습니다.");
+                    }
+
+                    if (ChkCommand(ref msg))
+                    {
+
+                        continue;
+                    }
+
+                    // 메시지 전송 및 버퍼 초기화
                     sw.WriteLine(msg);
                     sw.Flush();
+
+                    // 0.1초간 대기
+                    // 서버 과부하 방지용 코드
+                    Thread.Sleep(100);
                 }
             }
             catch
@@ -66,8 +108,14 @@ namespace Private
                 ns.Close();
                 tcpClient.Close();
             }
+
+            return;
         }
 
+        /// <summary>
+        /// 메세지를 받는 함수
+        /// </summary>
+        /// <param name="tcpClient">수신 받는 소켓</param>
         private static void RecvMessage(object tcpClient)
         {
 
@@ -77,12 +125,21 @@ namespace Private
 
             try
             {
+
+                // 메세지 수신 및 콘솔창에 출력
                 while (true)
                 {
 
                     message = sr.ReadLine();
 
-                    Console.WriteLine(message);
+                    // 채팅 입력 중이면 줄 띄움 실행
+                    if (Console.CursorLeft != 0)
+                    {
+
+                        Console.WriteLine();
+                    }
+
+                    ReadMessage(message);
                 }
             }
             catch (Exception ex)
@@ -100,20 +157,146 @@ namespace Private
             }
         }
 
-        private static string SetName()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static bool ChkCommand(ref string message)
         {
 
-            Console.Write("닉네임 : ");
-            string name = Console.ReadLine();
-
-            if (name == null || name == string.Empty)
+            if (message.Contains('|'))      // 명령어 입력에 방해될거 같아서 금지어 설정
             {
 
-                Random rand = new Random();
-                name = string.Format("User {0, 5}", rand.Next(1001, 65536));
+                ReadMessage("cr|특수문자 '|'는 사용할 수 없는 문자입니다.");
+                return true;
             }
-            Console.WriteLine($"{name}님 닉네임이 설정되었습니다.");
-            return name;
+
+            if (message[0] != '-')
+            {
+                return false;
+            }
+
+            string[] cmd = message.Split(' ');
+
+            try
+            {
+                if (cmd[0] == "-help")
+                {
+
+                    ReadMessage("cy|사용 가능한 명령어와 주의사항");
+                    ReadMessage("cy|-s name [원하는 닉네임]");
+                    ReadMessage("cy|닉네임의 경우 띄어쓰기가 불가능합니다.");
+                    ReadMessage("cy|-w [귓속말 대상] [귓속말 내용] ");
+                    ReadMessage("cy|귓속말은 파란색으로 표현됩니다.");
+                    Console.WriteLine();
+                    ReadMessage("cr|특수문자 '|'는 사용할 수 없는 문자입니다.");
+                    return true;
+                }
+                else if (cmd[0] == "-s")    // set의 약자로 사용
+                {
+
+                    if (cmd[1] == "name") // cmd[3]은 띄어쓰기가 있으면 강제로 예외 발생
+                    {
+
+                        message = $"sn|{cmd[2]}";
+                    }
+                }
+                else if (cmd[0] == "-w")    // whisper의 약자로 사용
+                {
+
+                    message = $"w|{cmd[1]}|{cmd[2]}";
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+
+                ReadMessage("cy|-help");
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// 수신한 메세지 읽기
+        /// </summary>
+        /// <param name="message">읽을 메세지</param>
+        private static void ReadMessage(string message)
+        {
+
+            string chk = message.Split('|')[0];
+
+            if (chk == message)
+            {
+
+                Console.WriteLine(message);
+            }
+            else if (chk == "cy")
+            {
+
+                SetColorString('y', message);
+            }
+            else if (chk == "cr")
+            {
+
+                SetColorString('r', message);
+            }
+            else if (chk == "cb")
+            {
+
+                SetColorString('b', message);
+            }
+            else if (chk == "cg")
+            {
+
+                SetColorString('g', message);
+            }
+            else
+            {
+
+                Console.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// 칼라 해석
+        /// </summary>
+        /// <param name="color">칼라 종류</param>
+        /// <param name="message">읽을 문자</param>
+        public static void SetColorString(char color, string message)
+        {
+
+            if (color == 'y')
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (color == 'r')
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (color == 'b')
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (color == 'g')
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine(message.Substring(3));
+
+            Console.ResetColor();
+
+            return;
         }
     }
 }

b45b9de201454134b659f8ceb212c0e760c94a30
Tryingpop <qotjdgns2001@gmail.com>
2023-07-09 16:05:18 +0900

    feat(greedy) : 2023. 7 Study 그리디 알고리즘 문제
    그리디 알고리즘은 미래를 생각하지 않고 현재에서 가장 최선의 선택을 하는
    기법
    
    그리디 알고리즘은 가장 좋은 정답을 찾는게 아니다
    그래서 근사 알고리즘이라고도 한다
    
    현재 2번 문제에서 막혔다
    알아볼건 이차원 배열, 다차원 배열에 대한 정의 부분을 다시 알아보고,
    C#에서 정렬하는 형식을 알아봐야한다

---
+++ BaekJoon/13/13_02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 9
+이름 : 배성훈
+내용 : 회의실 배정
+    문제번호 : 1931번
+*/
+
+namespace BaekJoon._13
+{
+    internal class _13_02
+    {
+
+        public static int SortTimes(int[] x, int[] y) 
+        {
+
+            int rs1 = x[0].CompareTo(y[0]);
+            int rs2 = x[1].CompareTo(y[1]);
+
+            return rs1 != 0? rs1 : rs2;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            int length = int.Parse(sr.ReadLine());
+
+            // 가변 배열
+            int[][] times = new int[length][];
+
+            for (int i = 0; i < length; i++)
+            {
+
+                times[i] = Array.ConvertAll(sr.ReadLine().Split(' '), item => int.Parse(item));
+            }
+
+
+        }
+    }
+}

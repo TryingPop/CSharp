e7d2d668ea59036438797a64e816a08ee84b5119
TryingPop <qotjdgns2001@gmail.com>
2023-07-10 18:02:04 +0900

    fix(greedy) : 2023. 7 Study 일단은 List를 이용해 정답 도출
    잘못된 로직으로 시행착오를 겪었다
    종료시간으로 먼저 오름차순 정렬 기준을 잡은 뒤,
    시작 시간으로 내림차순 정렬기준을 잡으면
    시작시간과 종료시간이 같은 회의를 먼저 실행하기에 최대값보다 작을 수 있다

--- BaekJoon/13/13_02.cs
+++ BaekJoon/13/13_02.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,33 +17,44 @@ namespace BaekJoon._13
     internal class _13_02
     {
 
-        public static int SortTimes(int[] x, int[] y) 
-        {
-
-            int rs1 = x[0].CompareTo(y[0]);
-            int rs2 = x[1].CompareTo(y[1]);
-
-            return rs1 != 0? rs1 : rs2;
-        }
-
-
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
 
             StreamReader sr = new StreamReader(Console.OpenStandardInput());
 
             int length = int.Parse(sr.ReadLine());
-
+            
             // 가변 배열
-            int[][] times = new int[length][];
+            // int[][] times = new int[length][];
+            List<long[]> times = new List<long[]>();
+
 
             for (int i = 0; i < length; i++)
             {
 
-                times[i] = Array.ConvertAll(sr.ReadLine().Split(' '), item => int.Parse(item));
+                times.Add(Array.ConvertAll(sr.ReadLine().Split(' '), item => long.Parse(item)));
+            }
+            sr.Close();
+
+            times = times.OrderBy(time => time[1]).ThenBy(time => time[0]).ToList();
+            // times = times.OrderBy(time => time[1]).ThenByDescending(time => time[0]).ToList();   // 이걸 사용하면 시작 시간 = 종료시간인 회의가 있는 경우
+                                                                                                    // 이를 먼저하기에 최대 숫자가 될 수 없다
+            int result = 0;
+            long time = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                
+                if (time <= times[i][0])
+                {
+
+                    result++;
+                    time = times[i][1];
+                }
             }
 
-
+            Console.WriteLine(result);
         }
     }
 }
+

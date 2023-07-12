85c1c5a97fe0db620600013409109faf20d21db7
Tryingpop <qotjdgns2001@gmail.com>
2023-07-12 17:34:27 +0900

    feat(dynamic) : 2023. 7 Study 동적 계획법
    접근 방법에 따라 표현법이 여러 개 될 수 있다
    가장 긴 증가하는 부분 수열 문제 참조!

---
+++ BaekJoon/14/14_12.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 12
+이름 : 배성훈
+내용 : 가장 긴 증가하는 부분 수열
+    문제번호 : 11053번
+*/
+
+namespace BaekJoon._14
+{
+    internal class _14_12
+    {
+
+        static void Main(string[] args)
+        {
+
+            int length = int.Parse(Console.ReadLine());
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), item => int.Parse(item));
+
+            int[] lens = new int[length];
+            int[] maxs = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+
+                if (i == 0)
+                {
+
+                    lens[i] = 1;
+                    maxs[i] = nums[0];
+                    continue;
+                }
+
+            }
+        }
+    }
+}

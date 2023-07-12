45fa65b07c6c98dc1649086d1f5c31e1b4233e0c
TryingPop <qotjdgns2001@gmail.com>
2023-07-13 00:03:55 +0900

    feat(dynamic) : 2023. 7 Study LCS 해결 방법부터 떠올려보자
    내일 점심까지가 시간 제한이고 안되면 검색 하기!

---
+++ BaekJoon/14/14_15.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 12
+이름 : 배성훈
+내용 : LCS
+    문제번호 : 9251번
+*/
+
+namespace BaekJoon._14
+{
+    internal class _14_15
+    {
+
+        static void Main15(string[] args)
+        {
+
+            string input0 = Console.ReadLine();
+            string input1 = Console.ReadLine();
+
+        }
+    }
+}

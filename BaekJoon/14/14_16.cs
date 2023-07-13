688e62414ffe3e59166b3ad3ca09f0015953911f
TryingPop <qotjdgns2001@gmail.com>
2023-07-13 22:05:35 +0900

    fix(dynamic) : 2023. 7 Study LCS 추론이 안되어서 결국 검색
    기존 아이디어에 대한 분석능력이 부족했던거 같다
    문자열 A와 B의 LCS를 찾는 방법을 다시 볼 경우 꼭 확인해봐야할거 같다

---
+++ BaekJoon/14/14_16.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 13
+이름 : 배성훈
+내용 : 평범한 배낭
+    문제번호 : 12865번
+*/
+namespace BaekJoon._14
+{
+
+    internal class _14_16
+    {
+
+        static void Main16(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int[] info = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+
+            int[][] inputs = new int[info[0]][];
+
+            for (int i =0;i < info[0]; i++)
+            {
+
+                inputs[i] = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            }
+
+            int[] memo = new int[info[0]];
+
+            for (int i = 0; i < info[0]; i++)
+            {
+
+                int weight = info[1];
+                for (int j = i; j < info[0]; j++)
+                {
+
+                    if (weight - inputs[j][0] >= 0)
+                    {
+
+                        
+                    }
+                }
+            }
+        }
+    }
+}

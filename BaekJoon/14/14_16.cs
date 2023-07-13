cda0fc4e0b3431e8fbfabd1c85e2a1ad2ae02027
TryingPop <qotjdgns2001@gmail.com>
2023-07-14 00:18:00 +0900

    feat(dynamic) : 2023. 7 Study 동적계획법
    동적계획법 문제를 풀면서 느낀 것은
    
    먼저 문제 자체의 규칙을 파악해야한다
    점화식으로 나타낼 수 있으면 점화식으로 나타내는게 좋다
    그러면 재귀함수 작성이 쉬워진다
    
    그리고 이를 이용해 어떤 변수를 메모리에 저장할지 설정
    여기서 설정한 변수에 따라 풀이방법, 메모리 양, 연산량이 모두 다르다
    
    그리고 과정이 중요하지 않으면 같은 정답을 도출하는 풀이로 변형해서
    푸는게 좋아보인다

--- BaekJoon/14/14_16.cs
+++ BaekJoon/14/14_16.cs
@@ -25,28 +25,76 @@ namespace BaekJoon._14
 
             int[][] inputs = new int[info[0]][];
 
+            int[] memo = new int[info[1] + 1];
+
             for (int i =0;i < info[0]; i++)
             {
 
                 inputs[i] = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
             }
 
-            int[] memo = new int[info[0]];
 
+            int count = 1;
             for (int i = 0; i < info[0]; i++)
             {
 
-                int weight = info[1];
-                for (int j = i; j < info[0]; j++)
+                for (int j = count - 1; j > 0; j--)
                 {
 
-                    if (weight - inputs[j][0] >= 0)
+
+                    // 비어있거나 인덱스 초과면 제외
+                    if (memo[j] == 0 || j + inputs[i][0] > info[1])
                     {
 
-                        
+                        continue;
+                    }
+
+                    // 기존 값 갱신이 필요하면 갱신 자기자신은 이후에 갱신한다
+                    if (memo[j + inputs[i][0]] < inputs[i][1] + memo[j])
+                    {
+
+                        memo[j + inputs[i][0]] = memo[j] + inputs[i][1];
+                    }
+
+                }
+
+                // 자기자신 갱신, 넣을 수 있는지 무게 검사
+                if (inputs[i][0] < memo.Length)
+                {
+
+                    // 값 갱신이 필요한지 확인
+                    if (memo[inputs[i][0]] == 0 || memo[inputs[i][0]] < inputs[i][1])
+                    {
+
+                        memo[inputs[i][0]] = inputs[i][1];
                     }
                 }
+
+                // for문 중 j를 이용하는 곳에서 for문을 적게 돌리기 위해 넣은 구문 count = memo.Length로 해도 상관 없다
+                if (count < memo.Length) count = count + inputs[i][0] < memo.Length ? count + inputs[i][0] : memo.Length;
+
+                /*
+                // 다른사람들 풀이는
+                int weight = inputs[i][0];
+                int value = inputs[i][1];
+
+                // 여기서 만들어지는 memo의 최대값이나 구하는 메모값이나 같다
+                for (int j = memo.Length - 1; j >= weight; j--)
+                {
+                    
+                    memo[weight] = Math.Max(memo[weight], memo[j - weight] + value);
+                }
+                */
             }
+
+            int result = 0;
+            for (int i = 0; i < memo.Length; i++)
+            {
+
+                if (result < memo[i]) result = memo[i];
+            }
+
+            Console.WriteLine(result);
         }
     }
 }

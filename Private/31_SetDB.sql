c35f8f9676ae4416d25b4f6a1c997cdd26eadaea
TryingPop <qotjdgns2001@gmail.com>
2023-07-16 01:01:09 +0900

    feat(db) : 2023. 7 Stduy 채팅 프로그램 데이터 베이스 쿼리문 세팅
    쿼리문 초기 세팅용이다

---
+++ Private/31_SetDB.sql
@@ -0,0 +1,17 @@
+# database 삭제
+userdbDROP DATABASE `userdb`;
+
+# database 생성
+CREATE DATABASE `userdb`;
+
+# 테이블 생성
+# 앞에서 만든 userdb 데이터베이스를 사용
+USE `userdb`;
+# uid, name, ip 를 정보를 갖는 user 테이블 생성
+CREATE TABLE `user` (
+	`uid`		VARCHAR(10),
+	`name`	VARCHAR(10),
+	`ip`		CHAR(15),
+	`port`	INT
+	);
+	

cbf6ad12e42099df8f31bf72f3b402b08d5b3d83
TryingPop <qotjdgns2001@gmail.com>
2023-07-17 20:46:52 +0900

    feat(stack) : 2023. 7 Study 스택 관련 문제
    Peek()메소드를 통해 스택에 맨 위에 잇는 원소를 제외하지 않고 확인할 수
    있다
    
    19_05의 경우 다른 사람의 풀이를 보니 큐를 이용할 필요 없고,
    인트형 변수를 써서 풀었다.

---
+++ BaekJoon/18/18_01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 15
+이름 : 배성훈
+내용 : 수 정렬하기 2
+    문제번호 : 2751번
+
+    병합정렬로 정렬하기!
+*/
+
+namespace BaekJoon._18
+{
+    internal class _18_01
+    {
+
+        static void Main1(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            int len = int.Parse(sr.ReadLine());
+            int[] nums = new int[len];
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < len; i++)
+            {
+
+                nums[i] = int.Parse(sr.ReadLine());
+            }
+            sr.Close();
+            
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            // 병합정렬 로직 !
+
+
+
+            Console.WriteLine(sb);
+
+            sw.Close();
+        }
+    }
+}

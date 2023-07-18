548cecac11ad24b41487ea8be17bb7b756b95db9
TryingPop <qotjdgns2001@gmail.com>
2023-07-19 02:45:02 +0900

    feat(baek) : 2023. 7 Study 집합과 맵 문제
    HashSet이 검색하는데 속도가 빠른거 같다

---
+++ BaekJoon/21/21_02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 19
+이름 : 배성훈
+내용 : 문자열 집합
+    문제번호 : 14425번
+*/
+
+namespace BaekJoon._21
+{
+    internal class _21_02
+    {
+
+        static void Main2(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int[] info = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            
+            HashSet<string> mySet = new HashSet<string>();
+            
+            for (int i = 0; i < info[0]; i++)
+            {
+
+                mySet.Add(sr.ReadLine());
+            }
+
+            int result = 0;
+            for (int i = 0; i < info[1]; i++)
+            {
+
+                if (mySet.Contains(sr.ReadLine()))
+                {
+
+                    result++;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

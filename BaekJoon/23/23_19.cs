00969cffe74e6c5ae463d9405ff9820d53350b19
TryingPop <qotjdgns2001@gmail.com>
2023-07-20 23:57:59 +0900

    feat(baek) : 2023. 7 Study 20단계 밑에 구멍난 문제
    문자열 입력 받는 부분만 주의하면 된다!
    또한 ascii의 13은 \r이다 줄 맨 앞으로 가라는 명령어이다

---
+++ BaekJoon/23/23_19.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 20
+이름 : 배성훈
+내용 : 그대로 출력하기
+    문제번호 : 11718번
+*/
+
+namespace BaekJoon._23
+{
+    internal class _23_19
+    {
+
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+#if false
+            int c;
+
+            while (!((c = Console.Read()) == -1))
+            {
+
+                if (c != '\r')
+                {
+
+                    sb.Append((char)c);
+                }
+            }
+
+#else
+            string str = "";
+            while (str != null)
+            {
+
+                str = Console.ReadLine();
+                sb.AppendLine(str);
+            }
+#endif
+            Console.WriteLine(sb);
+        }
+    }
+}

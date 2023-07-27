06ba3839abf38ddfd1548ea5b9ca49be9f68d8be
TryingPop <qotjdgns2001@gmail.com>
2023-07-27 19:49:55 +0900

    feat(book - tcp game) : 2023. 7 Study c#으로 온라인 게임 서버 만들기 깃 코드 따라 작성
    현재 CPacket까지는 이해했으나 CUserToken의 경우 부족한게 많아 무슨
    말인지 모르겠다.
    
    일단 따라 작성해보고 완성되서 다시 둘러보면서 기능을 알아보도록 해야겠다

---
+++ Private/30_온라인_게임_서버_만들기_01.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 7. 27
+이름 : 배성훈
+내용 : C#으로 온라인 게임 서버 만들기 (01)
+    한빛 미디어에서 출판한 책이다
+    여기에 사용된 코드를 분석하는 클래스
+
+    일단 저자의 소스코드를 여러 파트로 나눠서 그대로 따라 쳐본다
+    ---- 이후에는 윗글 생략! ----
+
+    여기서는 IPeer, CPacket을 작성한다
+
+    IPeer 인터페이스는 서버와 클라이언트 공통으로 작성되는 객체이다
+    메소드 이름으로 봐서 메세지를 보내고 연결을 담당하는 인터페이스로 보인다
+
+    CPacket 전송하는 데이터를 포멧시킨 형태같이 보인다
+
+    필요한 클래스 
+        IPeer : CPacket
+        CPacket : CUserToken, Defines
+*/
+
+namespace Private
+{
+    public partial class _30_온라인_게임_서버_만들기
+    {
+
+        /// <summary>
+        /// 서버와 클라이언트에서 공통으로 사용되는 세션 객체
+        /// 
+        /// 서버의 경우 :
+        ///     하나의 클라이언트 객체를 나타낸다
+        ///     이 인터페이스를 구현한 객체를 CNetworkService 클래스의 session_create_callback 호출 시 생성하여 리턴시켜 준다
+        ///     객체를 풀링할지 여부는 사용자가 원하는대로 구현한다
+        /// 
+        /// 클라이언트일 경우 :
+        ///     접속한 서버 객체를 나타낸다
+        /// </summary>
+        public interface IPeer
+        {
+
+            // 주석 처리된 코드
+            // void on_message(ArraySegment<byte> buffer);
+
+            // void process_user_operation(CPacket msg);
+
+            /// <summary>
+            /// CNetworkService.initialize에서 use_logicthread를 true로 설정할 경우
+            /// -> IO스레드에서 직접 호출됨
+            /// 
+            /// false로 설정할 경우
+            /// -> 로직 스레드에서 호출됨 
+            ///     로직 스레드는 싱글 스레드로 돌아감
+            /// </summary>
+            /// <param name="msg"></param>
+            void on_message(CPacket msg);
+
+            /// <summary>
+            /// 원격 연결이 끊겼을 때 호출된다
+            /// 이 메소드가 호출된 이후부터는 데이터 전송이 불가능하다
+            /// </summary>
+            void on_removed();
+
+            void send(CPacket msg);
+
+            void disconnect();
+        }
+
+        public class CPacket
+        {
+
+            public CUserToken owner { get; private set; }
+            public byte[] buffer { get; private set; }
+            public int position { get; private set; }
+            public int size { get; private set; }
+            public Int16 protocol_id { get; private set; }      // Int16 = short ( 2바이트 정수형 자료변수)
+
+            /// <summary>
+            /// CPacket 생성
+            /// </summary>
+            /// <param name="protocol_id"></param>
+            /// <returns></returns>
+            public static CPacket create(Int16 protocol_id)
+            {
+
+                CPacket packet = new CPacket();
+
+                // 주석도 따라쳤는데 의미가?
+                // todo: 다음 리팩토링 대상은 바로 여기이다 CPacketBufferManager?
+                // CPacket packet = CPacketBufferManager.pop();
+
+                packet.set_protocol(protocol_id);
+
+                return packet;
+            }
+
+            /// <summary>
+            /// 내용 없다
+            /// </summary>
+            /// <param name="packet"></param>
+            public static void destroy(CPacket packet)
+            {
+
+                // CPacketBufferManager.push(packet);
+            }
+
+            // ArraySegment는 배열의 부분을 가져오는 것이다
+            // 일종의 슬라이싱!
+            public CPacket(ArraySegment<byte> buffer, CUserToken owner)
+            {
+
+                // 참조로만 보관하여 작업한다
+                // 복사가 필요하면 별도로 구현해야 한다
+                this.buffer = buffer.Array;
+
+                // 헤더는 읽을 필요 없으니 그 이후부터 시작한다
+                this.position = Defines.HEADERSIZE;
+                this.size = buffer.Count;
+
+                // 프로토콜 아이디만 확일할 경우도 있으므로 미리 뽑아 놓는다
+                this.protocol_id = pop_protocol_id();
+                this.position = Defines.HEADERSIZE;
+
+                this.owner = owner;
+            }
+
+            public CPacket()
+            {
+
+                this.buffer = new byte[1024];
+            }
+
+            /// <summary>
+            /// 2바이트로 저장된 pop_protocol_id 읽기
+            /// 맨 앞에 2바이트가 protocol_id 같다
+            /// </summary>
+            /// <returns></returns>
+            public Int16 pop_protocol_id()
+            {
+
+                return pop_int16();
+            }
+
+            public void copy_to(CPacket target)
+            {
+
+                target.set_protocol(this.protocol_id);
+                target.overwrite(this.buffer, this.position);
+            }
+
+            /// <summary>
+            /// 대상이 배열을 해당 위치부터 끝까지 복사해서 
+            /// 내부 변수인 buffer에 담는다
+            /// 또한 내부 변수 position에 위치 정보를 담는다
+            /// </summary>
+            /// <param name="source">복사할 배열</param>
+            /// <param name="position">복사 시작 지점</param>
+            public void overwrite(byte[] source, int position)
+            {
+
+                Array.Copy(source, this.buffer, source.Length);
+                this.position = position;
+            }
+
+            /// <summary>
+            /// 버퍼에 있는 현재 위치 값 출력
+            /// </summary>
+            /// <returns></returns>
+            public byte pop_byte()
+            {
+
+                byte data = this.buffer[this.position];
+                this.position += sizeof(byte);
+                return data;
+            }
+
+            /// <summary>
+            /// 현재 버퍼의 위치 값 short로 읽기
+            /// </summary>
+            /// <returns>short 값</returns>
+            public Int16 pop_int16()
+            {
+
+                Int16 data = BitConverter.ToInt16(this.buffer, this.position);
+                this.position += sizeof(Int16);
+                return data;
+            }
+
+            /// <summary>
+            /// 버퍼의 현 위치 값 int로 읽기
+            /// </summary>
+            /// <returns>int 값</returns>
+            public Int32 pop_int32()
+            {
+
+                Int32 data = BitConverter.ToInt32(this.buffer, this.position);
+                this.position += sizeof(Int32);
+                return data;
+            }
+
+            /// <summary>
+            /// 문자열 읽기
+            /// 문자열 앞에는 문자열 길이 정보가 있다!
+            /// 문자열은 UTF8형식
+            /// </summary>
+            /// <returns>읽은 문자열</returns>
+            public string pop_string()
+            {
+
+                // 문자열 길이는 최대 2바이트 까지 0 ~32767
+                Int16 len = BitConverter.ToInt16(this.buffer, this.position);
+                this.position += sizeof(Int16);
+
+                // 인코딩은 utf8로 통일
+                string data = System.Text.Encoding.UTF8.GetString(this.buffer, this.position, len);
+                this.position += len;
+
+                return data;
+            }
+
+            /// <summary>
+            /// 현재 위치의 버퍼의 값 float으로 읽기
+            /// </summary>
+            /// <returns>읽은 float 값</returns>
+            public float pop_float()
+            {
+
+                float data = BitConverter.ToSingle(this.buffer, this.position);     // Single은 float의 다른 이름이라 보면 된다
+                this.position += sizeof(float);
+                return data;
+            }
+
+            /// <summary>
+            /// 프로토콜 넣는다
+            /// </summary>
+            /// <param name="protocol_id"></param>
+            public void set_protocol(Int16 protocol_id)
+            {
+
+                this.protocol_id = protocol_id;
+
+                // this.buffer = new byte[1024];
+
+                // 헤더는 나중에 넣을 것이므로 데이터 부터 넣을 수 있도록 위치를 점프 시켜 놓는다
+                this.position = Defines.HEADERSIZE;
+
+                push_int16(protocol_id);
+            }
+
+            /// <summary>
+            /// header + body 를 합한 사이즈를 입력
+            /// </summary>
+            public void record_size()
+            {
+
+                byte[] header = BitConverter.GetBytes(this.position);
+                header.CopyTo(this.buffer, 0);
+            }
+
+            // 밑에 같은 기능을 하는 push 함수가 있지만
+            // 프로토콜 id 부분을 강조하기 위해 정의해서 사용하는거 같다
+            /// <summary>
+            /// Int16 데이터를 버퍼에 추가한다
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push_int16(Int16 data)
+            {
+
+                byte[] temp_buffer = BitConverter.GetBytes(data);
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += temp_buffer.Length;
+            }
+
+            /// <summary>
+            /// 바이트 데이터를 버퍼에 추가
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push(byte data)
+            {
+
+                byte[] temp_buffer = BitConverter.GetBytes(data);
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += sizeof(byte);
+            }
+
+            /// <summary>
+            /// short 데이터를 버퍼에 추가한다
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push(Int16 data)
+            {
+
+                byte[] temp_buffer = BitConverter.GetBytes(data);
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += temp_buffer.Length;
+            }
+
+            /// <summary>
+            /// int 변수를 버퍼에 추가한다
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push(Int32 data)
+            {
+
+                byte[] temp_buffer = BitConverter.GetBytes(data);
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += temp_buffer.Length;
+            }
+
+            /// <summary>
+            /// 문자열을 버퍼에 추가한다
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push(string data)
+            {
+
+                // 문자열을 UTF8 형식으로 일코딩해 temp_buffer에 저장
+                byte[] temp_buffer = System.Text.Encoding.UTF8.GetBytes(data);
+
+                // 문자열의 길이 확인
+                Int16 len = (Int16)temp_buffer.Length;
+                // 문자열의 길이를 byte[] 형식으로 저장
+                byte[] len_buffer = BitConverter.GetBytes(len);
+
+                // 문자열의 길이를 먼저 버퍼에 넣는다
+                len_buffer.CopyTo(this.buffer, this.position);
+                this.position += sizeof(Int16);
+
+                // 이후에 문자열을 버퍼에 넣는다
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += temp_buffer.Length;
+            }
+
+            /// <summary>
+            /// 실수형 데이터를 버퍼에 넣는다
+            /// </summary>
+            /// <param name="data">추가할 데이터</param>
+            public void push(float data)
+            {
+
+                byte[] temp_buffer = BitConverter.GetBytes(data);
+                temp_buffer.CopyTo(this.buffer, this.position);
+                this.position += temp_buffer.Length;
+            }
+        }
+    }
+}
+
+// 참고할 사이트 
+// https://github.com/sunduk/FreeNet
+// 책의 설명이 중간에 누락된게 너무 많아서 깃으로 확인하기!
+
+// 이후에는 다음 코드 확인하자!
+// https://www.codeproject.com/Articles/83102/C-SocketAsyncEventArgs-High-Performance-Socket-Cod
---
+++ Private/33_온라인_게임_서버_만들기_02.cs
@@ -0,0 +1,572 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+/*
+날짜 : 2023. 7. 27
+이름 : 배성훈
+내용 : C#으로 온라인 게임 서버 만들기 (02)
+    CUserToken 과 Defines(CMessageResolver에 있다) 정보를 담고 있다!
+
+    Defines : 헤더 
+    CUserToken : ?
+
+    CUserToken : CMessageResolver, IMessageDispatcher, ClosedDelegate, CHeartbeatSender 가 필요
+*/
+
+namespace Private
+{
+    public partial class _30_온라인_게임_서버_만들기
+    {
+
+        /// <summary>
+        /// 헤더 사이즈의 크기를 보관
+        /// </summary>
+        class Defines
+        {
+
+            public static readonly short HEADERSIZE = 4;
+        }
+
+        public class CUserToken
+        {
+
+            /// <summary>
+            /// 토큰 상태
+            /// </summary>
+            enum State 
+            { 
+            
+                // 대기 중
+                Idle,
+
+                // 연결됨
+                Connected,
+
+                // 종료가 예약됨
+                // sending_list에 대기 중인 상태에서 disconnect를 호출한 경우,
+                // 남아있는 패킷을 모두 보낸 뒤 끊도록 하기 위한 상태 값
+                ReserveClosing,
+
+                // 소켓이 완전히 종료됨
+                Closed,
+            }
+
+            // 종료 요청 S -> C
+            const short SYS_CLOSE_REQ = 0;
+
+            // 종료 응답 C -> S
+            const short SYS_CLOSE_ACK = -1;
+
+            // 하트비트 시작 S -> C
+            public const short SYS_START_HEARTBEAT = -2;
+
+            // 하트비트 갱신 C -> S
+            public const short SYS_UPDATE_HEARTBEAT = -3;
+
+            // close 중복 처리 방지를 위한 플래그
+            // 0 = 연결된 상태
+            // 1 = 종료된 상태
+            int is_closed;
+
+            State current_state;
+
+            public Socket socket { get; set; }
+
+            public SocketAsyncEventArgs receive_event_args { get; private set; }
+            public SocketAsyncEventArgs send_event_args { get; private set; }
+
+            // 바이트를 패킷 형식으로 해석해주는 해석기
+            CMessageResolver message_resolver;
+
+            // session 객체
+            // 어플리케이션 딴에서 구현하여 사용
+            IPeer peer;
+
+            // BufferList 적용을 위해 queue에서 list로 변경
+            List<ArraySegment<byte>> sending_list;
+
+            // sending_list lock 처리에 사용되는 객체
+            private object cs_sending_queue;
+
+            IMessageDispatcher dispatcher;
+
+            public delegate void CloseDelegate(CUserToken token);
+            public ClosedDelegate on_session_closed;
+
+            // heartbeat
+            public long latest_heartbeat_time { get; private set; }
+
+            CHeartbeatSender heartbeat_sender;
+            bool auto_heartbeat;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="dispatcher"></param>
+            public CUserToken(IMessageDispatcher dispatcher)
+            {
+
+                this.dispatcher = dispatcher;
+                this.cs_sending_queue = new object();
+
+                this.message_resolver = new CMessageResolver();
+                this.peer = null;
+                this.sending_list = new List<ArraySegment<byte>>();
+                this.latest_heartbeat_time = DateTime.Now.Ticks;
+
+                this.current_state = State.Idle;
+            }
+
+
+            /// <summary>
+            /// 연결 및 연결에 맞는 상태 변경
+            /// </summary>
+            public void on_connected()
+            {
+
+                this.current_state = State.Connected;
+                this.is_closed = 0;
+                this.auto_heartbeat = true;
+            }
+
+            /// <summary>
+            /// peer 등록
+            /// </summary>
+            /// <param name="peer"></param>
+            public void set_peer(IPeer peer)
+            {
+
+                this.peer = peer;
+            }
+
+            /// <summary>
+            /// 수신과 전송 event 등록
+            /// </summary>
+            /// <param name="receive_event_args"></param>
+            /// <param name="send_event_args"></param>
+            public void set_event_args(SocketAsyncEventArgs receive_event_args, SocketAsyncEventArgs send_event_args)
+            {
+
+                this.receive_event_args = receive_event_args;
+                this.send_event_args = send_event_args;
+            }
+
+
+            // CMessageResolver 클래스를 추가하고 다시보기!
+            // 역할을 모르겠다. 
+            /// <summary>
+            /// 이 메소드에서 직접 바이트 데이터를 해석해도 되지만 Message resolver 클래스를 따로 둔 이유는
+            /// 추후에 확장성을 고려하여 다른 resolver를 구현할 때 CUserToken 클래스의 코드 수정을 최소화 하기 위함이ㄷㅏ?
+            /// 
+            /// ????
+            /// </summary>
+            /// <param name="buffer"></param>
+            /// <param name="offset"></param>
+            /// <param name="transfered"></param>
+            public void on_receive(byte[] buffer, int offset, int transfered)
+            {
+
+                this.message_resolver.on_receive(buffer, offset, transfered, on_message_completed);
+            }
+
+            /// <summary>
+            /// CMessageResolver 보낼 메소드
+            /// </summary>
+            /// <param name="buffer"></param>
+            void on_message_completed(ArraySegment<byte> buffer)
+            {
+
+                // peer 가 없으면 탈출
+                if (this.peer == null)
+                {
+
+                    return;
+                }
+
+
+                if (this.dispatcher != null)
+                {
+
+                    // 로직 스레드의 큐를 타고 호출되도록 함
+                    this.dispatcher.on_message(this, buffer);
+                }
+                else
+                {
+
+                    // IO 스레드에서 직접 호출
+                    CPacket msg = new CPacket(buffer, this);
+                    on_message(msg);
+                }
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="msg"></param>
+            public void on_message(CPacket msg)
+            {
+
+                // active close를 위한 코딩
+                // 서버에서 종료하라고 연락이 왔는지 체크한다
+                // 만약 종료신호가 맞다면 disconnect를 호출하여 받은쪽에서 먼저 종료 요청을 보낸다
+                switch (msg.protocol_id)
+                {
+
+                    // 종료 요청
+                    case SYS_CLOSE_REQ:
+                        disconnect();
+                        return;
+
+                    // 하트비트 시작 요청
+                    case SYS_START_HEARTBEAT:
+                        {
+
+                            // 순서대로 파싱해야 하므로 프로토콜 아이디는 버린다
+                            msg.pop_protocol_id();
+
+                            byte interval = msg.pop_byte();
+                            this.heartbeat_sender = new CHearbeatSender(this, interval);
+
+                            if (this.auto_heartbeat)
+                            {
+
+                                start_heartbeat();
+                            }
+                        }
+                        return;
+
+                    // 하트비트 업데이트 요청
+                    case SYS_UPDATE_HEARTBEAT:
+                        this.latest_heartbeat_time = DateTime.Now.Ticks;
+                        return;
+                }
+
+                // peer가 등록되어져 잇는 경우
+                if (this.peer != null)
+                {
+
+                    try
+                    {
+
+                        // protocol id 분석
+                        switch (msg.protocol_id)
+                        {
+
+                            // ?
+                            case SYS_CLOSE_ACK:
+                                this.peer.on_removed();
+                                break;
+
+                            // ?
+                            default:
+                                this.peer.on_message();
+                                break;
+                        }
+                    }
+                    catch (Exception)   // 에러 발생
+                    {
+
+                        close();
+                    }
+                }
+
+                // ACK 의미 찾아봐야한다
+                if (msg.protocol_id == SYS_CLOSE_ACK)
+                {
+
+                    // session_closed 가 비어져 있으면 지금 토큰의 세션 추가
+                    if (this.on_session_closed != null)
+                    {
+
+                        this.on_session_closed(this);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// ?
+            /// </summary>
+            public void close()
+            {
+
+                // 중복 수행을 막는다
+                if (Interlocked.CompareExchange(ref this.is_closed, 1, 0) == 1)
+                {
+
+                    return;
+                }
+
+                if (this.current_state == State.Closed)
+                {
+
+                    return;
+                }
+
+                this.current_state = State.Closed;
+                this.socket.Close();
+                this.socket = null;
+
+                this.send_event_args.UserToken = null;
+                this.receive_event_args.UserToken = null;
+
+                this.sending_list.Clear();
+                this.message_resolver.clear_buffer();
+
+                if (this.peer != null)
+                {
+
+                    CPacket msg = CPacket.create((short)-1);
+                    if (this.dispatcher != null)
+                    {
+
+                        this.dispatcher.on_message(this, new ArraySegment<byte>(msg.buffer, 0, msg.position));
+                    }
+                    else
+                    {
+
+                        on_message(msg);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// 패킷을 전송한다
+            /// 큐가 비어있을 경우에는 큐에 추가한 뒤에 바로 SendAsync 메소드를 호출하고,
+            /// 데이터가 들어있을 경우에는 새로 추가만 한다
+            /// 
+            /// 큐된 패킷의 전송 시점 :
+            ///     현재 진행 중인 SendAsync가 완료되었을 때 큐를 검사하여 나머지 패킷을 전송한다
+            /// </summary>
+            /// <param name="data"></param>
+            public void send(ArraySegment<byte> data)
+            {
+
+                lock (this.cs_sending_queue)
+                {
+
+                    this.sending_list.Add(data);
+
+                    if (this.sending_list.Count > 1)
+                    {
+
+                        // 큐에 무언가가 들어 있다면 아직 전송이 완료되지 않은 상태이므로 큐에 추가만하고 리턴한다
+                        // 현재 수행 중인 SendAsync가 완료된 이후에 큐를 검사하여 데이터가 있으면 SendAsync를 호출하여 전송해 줄 것이다
+                        return;
+                    }
+                }
+
+                start_send();
+            }
+
+            public void send(CPacket msg)
+            {
+
+                // 사이즈 입력
+                msg.record_size();
+                // 데이터 전달
+                send(new ArraySegment<byte>(msg.buffer, 0, msg.position));
+            }
+
+            /// <summary>
+            /// 비동기 전송을 시작한다
+            /// </summary>
+            void start_send()
+            {
+
+                try
+                {
+
+                    // 성능 향상을 위해 SetBuffer에서 BufferList를 사용하는 방식으로 변경함
+                    this.send_event_args.BufferList = this.sending_list;
+
+                    // 비동기 전송 시작
+                    bool pending = this.socket.SendAsync(this.send_event_args);
+                    if (!pending)
+                    {
+
+                        process_send(this.send_event_args);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    if (this.socket == null)
+                    {
+
+                        close();
+                        return;
+                    }
+
+                    Console.WriteLine("send error!! close socket. " + e.Message);
+                    throw new Exception(e.Message, e);
+                }
+            }
+
+            static int sent_count = 0;
+            static object cs_count = new object();
+
+            /// <summary>
+            /// 비동기 전송 완료 시 호출되는 콜백 메소드
+            /// </summary>
+            /// <param name="e"></param>
+            public void process_send(SocketAsyncEventArgs e)
+            {
+
+                if (e.BytesTransferred <= 0 || e.SocketError != SocketError.Success)
+                {
+
+                    // 연결이 끊겨서 이미 소켓이 종료된 경우일 것이다
+                    return;
+                }
+
+                lock (this.cs_sending_queue)
+                {
+
+                    // 리스트에 들어있는 데이터의 총 바이트 수
+                    var size = this.sending_list.Sum(obj => obj.Count);
+
+                    // 전송이 완료되기 전에 추가 전송을 요청했다면 sending_list에 무언가 더 들어있을 것이다
+                    if (e.BytesTransferred != size)
+                    {
+
+                        if (e.BytesTransferred < this.sending_list[0].Count)
+                        {
+
+                            string error = $"Need to send more! transferred {e.BytesTransferred}, packet size {size}";
+                            Console.WriteLine(error);
+
+                            close();
+
+                            return;
+                        }
+
+                        // 보낸 만큼 빼고 나머지 대기 중인 데이터들을 한방에 보내버린다
+                        int sent_index = 0;
+                        int sum = 0;
+                        for (int i = 0; i < this.sending_list.Count; ++i)
+                        {
+
+                            sum += this.sending_list[i].Count;
+                            if (sum <= e.BytesTransferred)
+                            {
+
+                                // 여기 까지는 전송 완료된 데이터 인덱스
+                                sent_index = i;
+                                continue;
+                            }
+
+                            break;
+                        }
+
+                        // 전송 완료된 것은 리스트에서 삭제한다
+                        this.sending_list.RemoveRange(0, sent_index + 1);
+
+                        // 나머지 데이터들을 한방에 보낸다
+                        start_send();
+                        return;
+                    }
+
+                    // 다 보냈고 더이상 보낼 것도 없다
+                    this.sending_list.Clear();
+
+                    // 종료가 예약된 경우, 보낼건 다 보냈으니 진짜 종료 처리를 진행한다
+                    if (this.current_state == State.ReserveClosing)
+                    {
+
+                        this.socket.Shutdown(SocketShutdown.Send);
+                    }
+                }
+            }
+
+            public void disconnect()
+            {
+
+                try
+                {
+
+                    if (this.sending_list.Count <= 0)
+                    {
+
+                        this.socket.Shutdown(SocketShutdown.Send);
+                        return;
+                    }
+
+                    this.current_state = State.ReserveClosing;
+                }
+                catch (Exception)
+                {
+
+                    close();
+                }
+            }
+
+            public void ban()
+            {
+
+                try
+                {
+
+                    byebye();
+                }
+                catch (Exception)
+                {
+
+                    close();
+                }
+            }
+
+            void byebye()
+            {
+
+                CPacket bye = CPacket.create(SYS_CLOSE_REQ);
+                send(bye);
+            }
+
+            public bool is_connected()
+            {
+
+                return this.current_state == State.Connected;
+            }
+
+            public void start_heartbeat()
+            {
+
+                if (this.heartbeat_sender != null)
+                {
+
+                    this.heartbeat_sender.play();
+                }
+            }
+
+            public void stop_heartbeat()
+            {
+
+                if (this.heartbeat_sender != null)
+                {
+
+                    this.heartbeat_sender.stop();
+                }
+            }
+
+            public void disable_auto_heartbeat()
+            {
+
+                stop_heartbeat();
+                this.auto_heartbeat = false;
+            }
+
+            public void update_heartbeat_manually(float time)
+            {
+
+                if (this.heartbeat_sender != null)
+                {
+
+                    this.heartbeat_sender.update(time);
+                }
+        }
+    }
+}

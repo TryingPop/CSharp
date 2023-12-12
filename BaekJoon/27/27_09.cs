f8b0e9b24b664afd459fd8259bf7706364d33a56
TryingPop <qotjdgns2001@gmail.com>
2023-12-12 18:22:07 +0900

    fix(baek) : 2023. 12 Study 백준 문제 푸는 중
    현재 오답 코드이다!
    답지를 보니 스택으로 푼다고 한다
    
    코드를 그냥 따라 입력해봤는데 잘못 친거 같다
    그래서 문제 해결 아이디어를 자세히 보고 직접 스스로 코드를 작성해야
    겠다!

--- BaekJoon/27/27_09.cs
+++ BaekJoon/27/27_09.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,15 @@ using System.Threading.Tasks;
 이름 : 배성훈
 내용 : 히스토그램에서 가장 큰 직사각형
     문제번호 : 6549번
+
+    그냥 이중 포문 돌리면 O(n^2) 이라 시간초과뜬다
+
+    여기서는 스택을 이용해 풀자!
+    
+    이건 추후에 해볼 방법
+    세그먼트 트리 + 분할 정복으로 푼다고 한다
+    세그먼트 트리는 구간합 or 값 변경 시간 복잡도가 n이 log n 으로 바뀐다 
+    대신 값 변경할 때 부모노드도 변경해야 하기에 시간 복잡도 1이 log n 으로 바뀐다
 */
 
 namespace BaekJoon._27
@@ -31,44 +42,48 @@ namespace BaekJoon._27
 
                     if (inputs[0] == 0) break;
 
-                    int[] lens = new int[inputs.Length];
-                    bool[] chk = new bool[inputs.Length];
-                    // 사각형 가로 길이 연산
-                    // O(n^2) 시간을 쓴다 시간초과 떴다
-                    // 다른 방법으로 해야한다!
-                    for (int i = 1; i <= inputs[0]; i++)
+                    Stack<int> idxs = new Stack<int>(inputs[0]);
+                    long max = 0;
+                    long min = int.MaxValue;
+
+                    for(int i = 1; i <= inputs[0]; i++)
                     {
 
-                        for (int j = 1; j <= i; j++)
+                        if (min > inputs[i]) min = inputs[i];
+
+                        while(idxs.Count != 0 && inputs[idxs.Peek()] > inputs[i])
                         {
 
-                            if (!chk[j])
+                            long height = inputs[idxs.Peek()];
+                            long width = i - idxs.Pop();
+
+                            if (max < width * height)
                             {
 
-                                if (inputs[i] >= inputs[j])
-                                {
-
-                                    lens[j]++;
-                                }
-                                else
-                                {
-
-                                    chk[j] = true;
-                                }
+                                max = width * height;
                             }
+                        }
+                        
+                        idxs.Push(i);
+                    }
+
+                    while(idxs.Count != 0)
+                    {
+
+                        long height = inputs[idxs.Peek()];
+                        long width = inputs[0] - idxs.Pop() + 1;
+
+
+                        if (max < width * height)
+                        {
+
+                            max = width * height;
                         }
                     }
 
+                    if (min * inputs[0] > max) max = min * inputs[0];
 
-                    long result = -1;
-                    for (int i = 1; i <= inputs[0]; i++)
-                    {
-
-                        long squareArea = inputs[i] * lens[i];
-                        if (result < squareArea) result = squareArea;
-                    }
-
-                    sb.AppendLine(result.ToString());
+                    sb.AppendLine(max.ToString());
                 }
             }
 
@@ -77,7 +92,6 @@ namespace BaekJoon._27
 
                 sw.Write(sb);
             }
-                
         }
     }
 }

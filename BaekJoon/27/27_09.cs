c0703739abdf74816b5185d99b89045591b52867
TryingPop <qotjdgns2001@gmail.com>
2023-12-12 19:27:30 +0900

    fix(baek) : 203. 12 Study 분할정복 문제 끝
    다만 마지막 문제는 분할 정복으로 다시 풀어봐야한다
    현재는 스택을 이용해서 해결했다

--- BaekJoon/27/27_09.cs
+++ BaekJoon/27/27_09.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
     그냥 이중 포문 돌리면 O(n^2) 이라 시간초과뜬다
 
     여기서는 스택을 이용해 풀자!
+    인덱스가 맞지 않아 자꾸 틀렸다
     
     이건 추후에 해볼 방법
     세그먼트 트리 + 분할 정복으로 푼다고 한다
@@ -27,7 +28,7 @@ namespace BaekJoon._27
     internal class _27_09
     {
 
-        static void Main(string[] args)
+        static void Main9(string[] args)
         {
 
             StringBuilder sb = new StringBuilder();
@@ -43,20 +44,36 @@ namespace BaekJoon._27
                     if (inputs[0] == 0) break;
 
                     Stack<int> idxs = new Stack<int>(inputs[0]);
+                    // 가장 큰 값
                     long max = 0;
-                    long min = int.MaxValue;
 
                     for(int i = 1; i <= inputs[0]; i++)
                     {
 
-                        if (min > inputs[i]) min = inputs[i];
-
+                        // 현재 지점 i의 값과 이전 지점 a의 값을 비교하는데,
+                        // 이전 지점 a의 값이 현재 지점 i의 값보다 큰 경우만 한번 확인
                         while(idxs.Count != 0 && inputs[idxs.Peek()] > inputs[i])
                         {
 
+                            // 이전 지점 a의 값을 높이로 한다
                             long height = inputs[idxs.Peek()];
-                            long width = i - idxs.Pop();
 
+                            // 이전 지점 a의 값을 높이로 하는 가장 면적이 큰 사각형을 찾는다
+                            // 가질 수 있는 최대 길이
+                            long width = i - 1;
+
+                            // 그리고 a 지점은 뺀다
+                            idxs.Pop();
+
+                            // a 값보다 작은 값을 갖는 b 지점이 존재하는지 확인
+                            if (idxs.Count != 0)
+                            {
+
+                                // b 지점이 있는경우 최대 길이는 a - b가 최대 길이가 된다
+                                width = i - 1 - idxs.Peek();
+                            }
+
+                            // 면적이 최대값인지 확인 
                             if (max < width * height)
                             {
 
@@ -64,15 +81,27 @@ namespace BaekJoon._27
                             }
                         }
                         
+                        // 현재 지점 i보다 이전 지점에서의 큰 값들 값을 모두 확인 했다
                         idxs.Push(i);
                     }
 
+                    // 남는 경우 해결
+                    // 최소 높이 지점의 판정과 남은 구간들이 여기에 있다
+                    // 만약 input이 1번 인덱스부터 증가하는 수열이면 여기서 모든연산이 이뤄진다.
                     while(idxs.Count != 0)
                     {
 
+                        // 앞과 같은 방법 이용
                         long height = inputs[idxs.Peek()];
-                        long width = inputs[0] - idxs.Pop() + 1;
+                        idxs.Pop();
+                        // 끝지점이 정해져있기에 여기서 가장 긴 길이는 전체 길이
+                        long width = inputs[0];
 
+                        if (idxs.Count != 0)
+                        {
+
+                            width = inputs[0] - idxs.Peek();
+                        }
 
                         if (max < width * height)
                         {
@@ -80,8 +109,6 @@ namespace BaekJoon._27
                             max = width * height;
                         }
                     }
-
-                    if (min * inputs[0] > max) max = min * inputs[0];
 
                     sb.AppendLine(max.ToString());
                 }

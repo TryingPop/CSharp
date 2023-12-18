85fbf6c41d342b30139eb4dc87cac89042664174
TryingPop <qotjdgns2001@gmail.com>
2023-12-18 17:39:28 +0900

    feat(baek) : 2023. 12 Study 동적 계획법 2 문제 풀이
    현재 마지막 30_04는 푸는 중이다

---
+++ BaekJoon/30/30_04.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2023. 12. 18
+이름 : 배성훈
+내용 : 양팔 저울
+    문제번호 : 2629번
+*/
+
+namespace BaekJoon._30
+{
+    internal class _30_04
+    {
+
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            sr.ReadLine();
+            int[] weights = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int len = int.Parse(sr.ReadLine());
+            int[] chks = sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            sr.Close();
+
+
+        }
+    }
+}

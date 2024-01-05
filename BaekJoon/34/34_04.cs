44be9206f016f73c3c946d990731389af0e29de4
TryingPop <qotjdgns2001@gmail.com>
2024-01-05 20:43:07 +0900

    feat(baek) : 2024. 1 Study 백준 투 포인트 단원
    현재 4번 문제 푸는 중이다
    해당 수 미만의 소수를 찾아야하는데, 소수들을 저장할지 고민 중이다

---
+++ BaekJoon/34/34_04.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 1. 5
+이름 : 배성훈
+내용 : 소수의 연속합
+    문제번호 : 1644번
+*/
+
+namespace BaekJoon._34
+{
+    internal class _34_04
+    {
+
+        static void Main4(string[] args)
+        {
+
+            
+            int MAX = int.Parse(Console.ReadLine());
+
+            bool[] notPrimes = new bool[MAX + 1];
+
+            int len = (int)Math.Sqrt(MAX) + 1;
+            for (int i = 2; i < len; i++)
+            {
+
+                if (notPrimes[i]) continue;
+
+                bool chk = false;
+                for (int j = 2; j <= MAX; j++)
+                {
+
+
+                    if (i * j > MAX) break;
+
+                    if (notPrimes[i * j]) continue;
+
+                    notPrimes[i * j] = true;
+                }
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}

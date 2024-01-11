a4f792fd31d036b3d174a012fe04b9309c3a1d1c
TryingPop <qotjdgns2001@gmail.com>
2024-01-11 22:55:48 +0900

    fix(comment) : 2024. 1 Study 백준
    ... 그냥 튜플로 푸는 것과 같은 방법이다.
    단지 int에서 char로 바꼈을 뿐이다;

---
+++ BaekJoon/35/35_07.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 1. 11
+이름 : 배성훈
+내용 : 최소비용 구하기 2
+    문제번호 : 11779번
+*/
+
+namespace BaekJoon._35
+{
+    internal class _35_07
+    {
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}

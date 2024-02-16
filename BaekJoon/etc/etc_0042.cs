5c770f64b56db5cdae928df70a584e1300011b19
TryingPop <qotjdgns2001@gmail.com>
2024-02-16 19:09:00 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    앞의 문제들 풀고 있다

---
+++ BaekJoon/etc/etc_0042.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. 16
+이름 : 배성훈
+내용 : 호석이 두 마리 치킨
+    문제번호 : 21278번
+
+    재미있어 보이는 문제다!
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0042
+    {
+
+        static void Main42(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int n = ReadInt(sr);
+
+
+            int l = ReadInt(sr);
+
+
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int ret = 0;
+            int c;
+
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}

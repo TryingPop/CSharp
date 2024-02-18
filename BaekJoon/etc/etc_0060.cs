7f185b432c5741dda5567d64e58a13b8bfda63c8
TryingPop <qotjdgns2001@gmail.com>
2024-02-19 03:42:53 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    BFS? 탐색과, 다익스트라 문제다

---
+++ BaekJoon/etc/etc_0060.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. ?
+이름 : 배성훈
+내용 : 빌런 호석
+    문제번호 : 22251번
+
+    제목에 끌렸다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0060
+    {
+    }
+}
---
+++ BaekJoon/etc/etc_0061.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. ?
+이름 : 배성훈
+내용 : 수 묶기
+    문제번호 : 1744번
+
+    재미있어 보이는 문제?
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0061
+    {
+    }
+}

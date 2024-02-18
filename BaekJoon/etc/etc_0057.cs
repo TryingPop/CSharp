a6153c1efdf823c93cf48d186a8e8c10455d5e40
TryingPop <qotjdgns2001@gmail.com>
2024-02-18 21:29:43 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    실버 ~ 골드 문제다
    이분 탐색, 해시, 유니온 파인드 알고리즘을 쓰는 문제다

---
+++ BaekJoon/etc/etc_0057.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. 18
+이름 : 배성훈
+내용 : 파티
+    문제번호 : 1238번
+
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0057
+    {
+
+        static void Main57(string[] args)
+        {
+
+ 
+        }
+    }
+}

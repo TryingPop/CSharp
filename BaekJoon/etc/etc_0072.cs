bc72a1b6c592af54aa02e4183de75897eb7cff5f
TryingPop <qotjdgns2001@gmail.com>
2024-02-20 21:17:08 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    나락도 락이다
    dp를 쓰는 문제다!

---
+++ BaekJoon/etc/etc_0072.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. 20
+이름 : 배성훈
+내용 : 로또
+    문제번호 : 6603번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0072
+    {
+
+    }
+}

e49883b24befb1c2009b92261b123aca953729e3
TryingPop <qotjdgns2001@gmail.com>
2024-02-22 02:16:21 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    실버 난이도의 간단한 문제들이다

--- BaekJoon/etc/etc_0074.cs
+++ BaekJoon/etc/etc_0074.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
 날짜 : 2024. 2. -
@@ -10,8 +8,15 @@ using System.Threading.Tasks;
 내용 : 방탈출
     문제번호 : 23352번
 
-    재미있어 보이는 문제다!
-    완전 탐색을 해야한다!
+    해당 방법을 푸는 알고리즘을 몰라서 힌트를 봤고, 
+    브루트포스가 있어 완전 탐색으로 풀었다
+
+    아이디어는 다음과 같다
+    각 점에서 가장 긴 길이에 큰값을 찾는다!
+    그리고 길이가 길면 무조건 값을 갱신하고, 길이가 같을 때는 값 비교를 해서 큰 값으로 갱신한다
+    탐색 방법은 길이 갱신을 확인하기 쉬운 BFS 탐색을 이용했다
+
+    해당 방법으로 제출하니 이상없이 통과했다
 */
 
 namespace BaekJoon.etc
@@ -42,10 +47,81 @@ namespace BaekJoon.etc
 
             int[,] dp = new int[row, col];
 
-            // 최대 거리를 저장하자~
-            Queue<(int r, int c)> q = new();
+            int[] dirX = { -1, 1, 0, 0 };
+            int[] dirY = { 0, 0, -1, 1 };
 
+            Queue<(int r, int c)> q = new(row * col);
+            int maxLen = 0;
+            int max = 0;
+            for (int r = 0; r < row; r++)
+            {
 
+                for (int c = 0; c < col; c++)
+                {
+
+                    if (board[r, c] == 0) continue;
+
+                    q.Enqueue((r, c));
+
+                    dp[r, c] = 1;
+                    int chk = 1;
+                    int find = 0;
+
+                    while(q.Count > 0)
+                    {
+
+                        var node = q.Dequeue();
+                        int curLength = dp[node.r, node.c];
+                        int curVal = board[node.r, node.c];
+                        if (curLength > chk)
+                        {
+
+                            // 길이 갱신 -> 큰값도 갱신!
+                            chk = curLength;
+                            find = curVal;
+                        }
+                        else if (find < curVal) find = curVal;
+
+                        for (int i = 0; i < 4; i++)
+                        {
+
+                            int nextR = node.r + dirY[i];
+                            int nextC = node.c + dirX[i];
+                            if (ChkInvalidPos(nextR, nextC, row, col)) continue;
+
+                            if (board[nextR, nextC] == 0 || dp[nextR, nextC] != 0) continue;
+
+                            dp[nextR, nextC] = curLength + 1;
+                            q.Enqueue((nextR, nextC));
+                        }
+                    }
+
+                    // 1칸 이상 탐색했다!
+                    if (chk > 1) find += board[r, c];
+
+                    if (maxLen < chk) 
+                    { 
+                        
+                        // 최대 길이가 늘어나는경우다
+                        maxLen = chk;
+                        max = find;
+                    }
+                    // 
+                    else if (chk == maxLen && max < find) max = find;
+
+                    for (int i = 0; i < row; i++)
+                    {
+
+                        for (int j = 0; j < col; j++)
+                        {
+
+                            dp[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
         }
 
         static bool ChkInvalidPos(int _r, int _c, int _row, int _col)

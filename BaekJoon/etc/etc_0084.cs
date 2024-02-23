175633cc5dfc843b36f68c4b639c9b256f46cd45
TryingPop <qotjdgns2001@gmail.com>
2024-02-24 01:07:02 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    브루트 포스 문제다

---
+++ BaekJoon/etc/etc_0084.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. 24
+이름 : 배성훈
+내용 : 연구소
+    문제번호 : 14502번
+*/
+namespace BaekJoon.etc
+{
+    internal class etc_0084
+    {
+    }
+}
---
+++ BaekJoon/etc/etc_0085.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. 24
+이름 : 배성훈
+내용 : 생태학
+    문제번호 : 4358번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0085
+    {
+    }
+}

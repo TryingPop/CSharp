f5b7787bc1994cce44c5013264df65c8210338f5
TryingPop <qotjdgns2001@gmail.com>
2024-02-25 19:59:03 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제

---
+++ BaekJoon/etc/etc_0092.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. -
+이름 : 배성훈
+내용 : Projects
+    문제번호 : 9118번
+
+    ?
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0092
+    {
+    }
+}
---
+++ BaekJoon/etc/etc_0096.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 2. -
+이름 : 배성훈
+내용 : 비밀번호
+    문제번호 : 13908번
+
+    만약 빠른 시간에 풀어야한다면 수학적으로 풀거 같다
+    그것이 아니라면, 그냥 브루트 포스로 풀 것이다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0096
+    {
+
+        static void Main96(string[] args)
+        {
+
+
+        }
+    }
+}

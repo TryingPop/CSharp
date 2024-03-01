01dc811f653636c73369b02aecc9f6b830c070e1
TryingPop <qotjdgns2001@gmail.com>
2024-03-01 22:45:39 +0900

    feat(test) : 2024. 2 Study 백준 연습용 문제
    etc_0117은 구현 문제다 일일히 색칠된 구간 확인해서
    없다가 있거나 있다가 없는 길이를 확ㅇ니했다
    
    etc_0132는 간단한 배열 문제다
    그러나 입력이 많아 시간이 오래 걸린다
    buffer를 조절해 1초 안으로 줄였다
    
    etc_0135 는 완전탐색 문제다
    접근 로직을 잘못짜서 많이 틀렸다
    
    etc_0136 는 아직 풀이방법이 안떠오른다
    
    etc_0137 는 수학?문제다 문제 해석이 중요하다
    
    etc_0138 은 투포인트 문제인데 바로 안떠올라 시간이 오래 걸렸다
    
    etc_0139 는 다익스트라 문제다 입력 수가 적어 여러 방법으로 풀린다
    이분 탐색을 적용하면 더 빨라지나, 이분탐색 방법은 내일 접근해봐야겠다

---
+++ BaekJoon/etc/etc_0136.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 1
+이름 : 배성훈
+내용 : 수강변경
+    문제번호 : 23305번
+
+    풀이가 바로 안떠오른다;
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0136
+    {
+
+        static void Main136(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int len = ReadInt(sr);
+
+            int[] man = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+
+                man[i] = ReadInt(sr);
+            }
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int c, ret = 0;
+
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}

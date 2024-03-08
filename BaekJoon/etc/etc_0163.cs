43fbd1982fa0bda4c894f2b93a4922b7ebb16530
TryingPop <qotjdgns2001@gmail.com>
2024-03-08 17:45:02 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0163은 아직 못푼 문제
    A,B의 min 값이 2라 승리 조건부터 확인해야한다
    
    etc_0164은 관계를 구현? 하는 문제다
    거리 처럼 했다

---
+++ BaekJoon/etc/etc_0163.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 8
+이름 : 배성훈
+내용 : 카르텔 님 게임
+    문제번호 : 30688번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0163
+    {
+
+        static void Main163(string[] args)
+        {
+
+            StreamReader sr = new(Console.OpenStandardInput());
+
+            int N = ReadInt(sr);
+            // 짝수 고정
+            int K = ReadInt(sr);
+
+            sr.Close();
+
+            // 승리 전략을 다시 짜봐야한다
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int c, ret = 0;
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_0164.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 8
+이름 : 배성훈
+내용 : 결혼식
+    문제번호 : 5567번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0164
+    {
+
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int n = ReadInt(sr);
+
+            List<int>[] friends = new List<int>[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                friends[i] = new();
+            }
+
+            int len = ReadInt(sr);
+
+            for (int i = 0; i < len; i++)
+            {
+
+                int f = ReadInt(sr);
+                int b = ReadInt(sr);
+
+                friends[f].Add(b);
+                friends[b].Add(f);
+            }
+
+            sr.Close();
+
+            Queue<int> q = new Queue<int>();
+            bool[] accept = new bool[n + 1];
+
+            accept[1] = true;
+
+            for (int i = 0; i < friends[1].Count; i++)
+            {
+                
+                q.Enqueue(friends[1][i]);
+            }
+
+            while (q.Count > 0)
+            {
+
+                var node = q.Dequeue();
+                accept[node] = true;
+
+                for (int i = 0; i < friends[node].Count; i++)
+                {
+
+                    int idx = friends[node][i];
+                    accept[idx] = true;
+                }
+            }
+
+            int ret = -1;
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (accept[i]) ret++;
+            }
+
+            Console.WriteLine(ret);
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int c, ret = 0;
+
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}

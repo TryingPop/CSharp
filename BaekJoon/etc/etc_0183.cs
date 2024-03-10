ed9bf6cd0e330169ff3071b3d118b9a98b287ba2
TryingPop <qotjdgns2001@gmail.com>
2024-03-11 03:52:46 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0181 해시를 쓰는 문제다
    etc_0182 그리디 문제다 동형인 상황으로 풀면된다
    etc_0183 나중에 풀 문제다
    etc_0184 배낭 문제다 한번에 코드로 구현 못해 연습이 필요한 단원이다
    etc_0185 수학 문제다
    etc_0186 조건 확인 문제다

---
+++ BaekJoon/etc/etc_0183.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 11
+이름 : 배성훈
+내용 : 전구 상태 바꾸기
+    문제번호 : 30023번
+
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0183
+    {
+
+        static void Main183(string[] args)
+        {
+
+
+        }
+    }
+}

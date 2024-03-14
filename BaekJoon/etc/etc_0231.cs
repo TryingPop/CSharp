3df3138375dcff53ebbeaa26ea970aee9219c112
TryingPop <qotjdgns2001@gmail.com>
2024-03-15 08:00:42 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0227 조건부 확률 문제다
    필요한 부분만 메모리에 저장해서 풀 수 있으나,
    전체 경우를 기록해서 문제를 푼 뒤 필요한 부분만 추출해서 다음으로
    제출했다
    
    etc_0228 DFS + 브루트 포스로 풀었다
    중복 경우가 있어 효율적이진 않다
    
    etc_0229 구현 문제다
    반례 하나 캐치 못해서 엄청나게 틀렸다
    
    etc_0230 브루트포스 백트래킹 문제다
    DFS로 구현했고, 이전 좌표를 받아와서 중복 안되게 했다
    
    etc_0231 오후에 풀 문제다
    
    etc_0232 구현문제다
    N^2의 시간이 나온다
    
    etc_0233 수학, 뤼카 정리 문제다
    etc_0232의 범위만 확장된 문제인데 원리를 알아야 빠른시간에 도출할 수
    있다
    
    etc_0234 비둘기집 원리 문제다
    33을 넘으면 비둘기집 원리로 바로 답이 도출되고 32 이하인 경우는
    DFS로 브루트포스로 최선의 수를 찾았다
    
    etc_0235 그리디 문제다
    해시를 써서 카운팅과 최대값을 해서 제출했다
    
    etc_0236 구현 문제다
    조건대로 하드코딩으로 구현했다

---
+++ BaekJoon/etc/etc_0231.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 15
+이름 : 배성훈
+내용 : 결혼식
+    문제번호 : 2317번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0231
+    {
+    }
+}

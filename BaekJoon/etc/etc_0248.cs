acb36674aef64fa690b51110b32a46880cb7dd5e
TryingPop <qotjdgns2001@gmail.com>
2024-03-16 08:25:39 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0239 다익스트라 쓰는 최단경로 문제다
    
    etc_0240 비트마스킹 수학 문제다
    정렬된 문제이고 2개로 변환하는 점에서 비트마스킹을 쓸 수 있다
    
    etc_0241 수학 문제다
    dp를 써서 풀 수 있으나 확률 계산하면 그냥 풀 수 있다
    
    etc_0242 동적계획법 문제다
    그리디하게 접근해서 풀었다
    
    etc_0243 나중에 풀 문제다
    
    etc_0244 수학 문제다
    손 계산을 통해 식을 구했고, 99%를 캐치 못해 여러번 틀렸다
    
    etc_0245 브루트포스, 백트래킹으로 푼 문제다
    
    etc_0246 그리디 문제다
    닭을 빠르게 소에게 배정하는게 중요하다
    
    etc_0247 그리디 문제다
    가장 나중에 사용하는 것을 먼저 뽑는게 중요하다
    
    etc_0248 나중에 풀 문제다
    
    etc_0249 구현, 브루트 포스 문제다
    DFS를 이용해 브루트 포스로 풀었다

---
+++ BaekJoon/etc/etc_0248.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 16
+이름 : 배성훈
+내용 : 되돌리기
+    문제번호 : 1360번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0248
+    {
+    }
+}

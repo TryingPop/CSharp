7ac4aa4b40cf454299cced8d8e66a472bf23c3e1
TryingPop <qotjdgns2001@gmail.com>
2024-03-19 20:25:14 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0280 그리디, 정렬 문제다
    etc_0281 그리디, 구현, 시뮬레이션 문제다
    오버플로우를 생각못해 한참을 틀렸다
    
    etc_0282 구현, 시뮬레이션 문제다
    etc_0283 구현, 시뮬레이션 문제다
    
    etc_0284 그리디, 정렬, 누적합 문제다
    사탕의 최대한 담은 상태에서 당도 최대값을 구하는 문제인줄 알고
    한참을 틀렸다 찾아보니 당도만 높으면 되는 문제였다
    
    etc_0285 그디리 알고리즘 문제다
    etc_0286 문자열, 구현 문제다
    etc_0287 사칙연산 문제다
    etc_0288 브루트포스 백트래킹 문제다
    etc_0289 그리디 알고리즘 문제다
    etc_0290 풀 문제다

---
+++ BaekJoon/etc/etc_0290.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 19
+이름 : 배성훈
+내용 : 거짓말
+    문제번호 : 1043번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0290
+    {
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}

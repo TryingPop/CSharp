a6c96f7eea45283d6df5bee007a74a656be3955c
TryingPop <qotjdgns2001@gmail.com>
2024-03-20 21:57:30 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0280 그리디 정렬 문제다
    유클리드 알고리즘처럼 문자열을 정렬하면 된다
    그런데 이는 a + b 와 b + a를 비교하는 것과 같다
    
    etc_0290 분리집합 문제다
    처음에는 벨만 - 포드 알고리즘의 로직을 써서 풀었다
    해당 로직은 특정 노드에서 시작하여 간선을 이어가는 과정을
    노드의 수만큼 반복하면 모든 노드들의 연결관계가 확인된다는 로직이다
    (벨만 포드는 이를 이용해 한 번 더 돌려 음의 사이클을 발견한다)
    그런데, 풀고나서 힌트를 보니, 유니온 파인드를 쓸 수 있음을 알게되고
    유니온 파인드로 다시 풀어 제출했다
    
    etc_0291 문자열, 브루트포스 알고리즘 문제다
    조건대로 구현하니 이상없이 통과했따
    etc_0292 그리디 구현 문제다 그리디하게 풀었다
    etc_0293 구현, 문자열 문제다
    두 포인터 알고리즘을 써서 비교하고 딕셔너리로 저장했다
    etc_0294 구현, 문자열, 시뮬레이션 문제다
    문제 규칙을 찾아 결과를 바로 입력했다
    
    etc_0295 브루트포스 비트마스킹 문제다
    처음에 비트마스킹 아이디어가 안떠올라 배열을 이용했고,
    다 풀고나서 힌트를 보고 비트마스킹으로 풀 수 있음을 확인했다
    그래서 비트마스킹으로 다시 풀어서 재출했다
    
    etc_0296 스택, 자료구조, 구현, 시뮬레이션 문제다
    처음에 어떻게 접근할지 몰라 힌트를 봤다
    스택 힌트를 보고 아이디어가 떠올라 풀어냈다
    그리고 자료구조는 최대힙을 이용해야하는데, 따로 만드는거보다
    우선순위 큐의 비교방법만 변환하여 사용했다
    
    etc_0297 구현, 시뮬레이션 문제다
    큐를 이용해 풀었다
    
    etc_0298 BFS 탐색문제다
    방문여부를 보드에 숫자값으로 확인하며 풀려고 했는데
    코드만 복잡해지고, 로직 만드는 것도 복잡해져서 여러 번 틀렸다
    결국 방문 여부 bool 배열을 따로 만들었고
    제출해서 통과했다
    
    etc_0299 나중에 풀 문제다
    etc_0300 나중에 풀 문제다
    etc_0301 그리디 문제다

---
+++ BaekJoon/etc/etc_0299.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 20
+이름 : 배성훈
+내용 : 동전
+    문제번호 : 3363번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0299
+    {
+
+        static void Main299(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            bool[] chk = new bool[3];
+
+            int[][] calc = new int[3][];
+            for (int i = 0; i < 3; i++)
+            {
+
+                calc[i] = new int[13];
+            }
+
+            int[] left = new int[4];
+            int[] right = new int[4];
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                int l = 0;
+                int r = 0;
+                for (int j = 0; j < 4; j++)
+                {
+
+                    int cur = ReadInt(sr);
+                    left[j] = cur;
+                    l += cur;
+                }
+
+                int op = ReadInt(sr);
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    int cur = ReadInt(sr);
+                    right[j] = cur;
+                    r += cur;
+                }
+
+                bool isTrue = true;
+                bool isL = true;
+                if (op == '<' - '0') isTrue = false;
+                else if (op == '>' - '0') 
+                { 
+                    
+                    isTrue = false; 
+                    isL = false; 
+                }
+
+                for (int j = 0; j < 4; j++)
+                {
+
+                    calc[i][left[j]] = isTrue ? 1 : isL ? 2 : 3;
+                    calc[i][right[j]] = isTrue ? 1 : isL ? 3 : 2;
+                }
+
+                for (int j = 1; j < 13; j++)
+                {
+
+                }
+
+            }
+
+            Console.WriteLine();
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int c, ret = 0;
+
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}

bb3d6cdefbe42ec26373a637b46f6d6c1939e809
TryingPop <qotjdgns2001@gmail.com>
2024-03-23 17:22:00 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0213 조금 있다가 풀어볼 문제다
    etc_0299 나중에 풀 문제다
    etc_0302 그리디, 정렬, 우선순위 큐 문제다
     etc_0308 문제의 아이디어를 써서 풀었다
    
    etc_0332 구현, 브루트포스 문제다
    etc_0333 구현, 브루트포스 문제다
    etc_0334 BFS 문제다
    visit 처리를 안해서 시간초과 떳었다
    
    etc_0335 bfs, dfs문제다
    bfs로 해결했다
    
    etc_0336 트리, dfs 문제다
    앞에서 리프 찾는 아이디어를 간선의 개수로 하니 쉽게 풀렸다
    
    etc_0337 구현, 많은 조건 분기 문제다
    조건 분석을 제대로 안해 2번 틀렸다
    -가 한개인 경우 컴파일 에러 나지 않는다!
    
    etc_0338 구현 문제다
    어셈블리어에 관련된 설명이 있다
    
    etc_0339 그리디 알고리즘, dfs문제다
    먼저 bfs와 dp를 이용해 풀었다
    
    etc_0340 구현, 브루트포스, 기하학, 많은 조건 분기 문제다
    그리디하게 접근하면 아이디어 찾는건 쉽다
    다만 조건 분기 쪽에서 예쁘게 코드를 짜볼려다가 여러 번 틀리고
    if문 8개로 풀었다

--- BaekJoon/etc/etc_0213.cs
+++ BaekJoon/etc/etc_0213.cs
@@ -15,5 +15,42 @@ namespace BaekJoon.etc
 {
     internal class etc_0213
     {
+
+
+    }
+
+    struct Pos : IComparable<Pos>
+    {
+
+        public int x;
+        public int y;
+        public bool chk;
+
+        public int CompareTo(Pos other)
+        {
+
+            int ret = x.CompareTo(other.x);
+            if (ret != 0) return ret;
+
+            ret = y.CompareTo(other.y);
+            return ret;
+        }
+
+        public void Set(int _x, int _y)
+        {
+
+            x = _x;
+            y = _y;
+        }
+
+        public bool ChkArea(int _x, int _y, int _size)
+        {
+
+            if (chk) return true;
+            if (x < _x || _x + _size < x) return false;
+            if (y < _y || _y + _size < y) return false;
+
+            return true;
+        }
     }
 }

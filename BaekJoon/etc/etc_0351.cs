f78ca4cab78cd5288856b514d5080015b0ace4fe
TryingPop <qotjdgns2001@gmail.com>
2024-03-25 17:22:56 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0213 이분탐색 문제다
    etc_0341 구현, 누적합 문제다
    etc_0342 이분탐색 문제다
    etc_0343 구현, 문자열, 파싱 문제다
    etc_0344 수학, 그리디, 정렬 문제다
    etc_0345 그리디 문제다
    etc_0346 구현, 문자열, 해시 문제다
    etc_0347 구현 문제다
    etc_0348 구현, 브루트 포스 문제다
    etc_0349 dp문제다
    etc_0350 문자열, 누적합, 스위핑, 파싱 문제다
    etc_0351 현재 풀고 있는 문제다 -> 이차원 배열의 누적합을 필요로 하는데
    처음 구현해봐서 조심히 하고 있다
    etc_0352 수학, 브루트포스, 정수론 문제다
    etc_0353 아직 안푼 문제다

---
+++ BaekJoon/etc/etc_0351.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. -
+이름 : 배성훈
+내용 : 사과나무
+    문제번호 : 20002번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0351
+    {
+
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int n = ReadInt();
+
+            int[,] board = new int[n, n];
+
+            for (int r = 0; r < n; r++)
+            {
+
+                for (int c = 0; c < n; c++)
+                {
+
+                    board[r, c] = ReadInt();
+                }
+            }
+
+            sr.Close();
+
+            int[,] rSum = new int[n + 1, n + 1];
+            int[,] cSum = new int[n + 1, n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+
+                    cSum[i + 1, j + 1] = board[i, j];
+                    cSum[i + 1, j + 1] += cSum[i + 1, j];
+
+                    rSum[j + 1, i + 1] = board[j, i];
+                    rSum[j + 1, i + 1] += rSum[j, i + 1];
+                }
+            }
+
+            int max = -1_000;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int first = 0;
+                for (int j = 0; j < i; j++)
+                {
+
+                    first += cSum[i, j + 1];
+                }
+
+
+                for (int r = 0; r <= n - i; r++)
+                {
+
+                    if (r > 0)
+                    {
+
+
+                    }
+
+                    int calc = first;
+
+                    if (max < calc) max = calc;
+                    for (int c = 1; c <= n - i; c++)
+                    {
+
+
+                    }
+                }
+            }
+
+
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                bool plus = true;
+                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    else if (c == '-')
+                    {
+
+                        plus = false;
+                        continue;
+                    }
+
+                    ret = ret * 10 + c - '0';
+                }
+
+                return plus ? ret : -ret;
+            }
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_0353.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 24
+이름 : 배성훈
+내용 : 카드게임
+    문제번호 : 10835번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0353
+    {
+    }
+}

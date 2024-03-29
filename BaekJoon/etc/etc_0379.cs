bde9e76db6e1cc3669718089824b8ef271a74472
TryingPop <qotjdgns2001@gmail.com>
2024-03-29 17:01:34 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0376 플로이드 워셜 문제다
    구슬의 무게 순서로 간선을 만들고 앞에 몇 개, 뒤에 몇 개를 세어 풀었다
    etc_0377 구현, 브루트포스 문제다
    조건대로 구현했다
    etc_0378 그리디, 정렬 문제다
    etc_0379 해당 문제는 FFT알고리즘이 필요해 보류한다
    etc_0380 구현, 정렬, 슬라이딩 윈도우 문제다
    슬라이딩 윈도우 기법 대신 브루트포스로 해도될 정도의 크기라
    브루트포스로 했다
    
    etc_0381 수학, 구현 문제다
    etc_0382 구현, 문자열, 브루트포스 문제다
    etc_0383 수학, 정렬, 브루트포스 문제다
    etc_0384 위상정렬, 해시 문제다
    이름을 인덱스로 바꾸는데 해시가 쓰이고, 부모 자식관계를 간선으로 생각해
    위상정렬 해주면 된다
    
    etc_0385 그리디, 브루트포스 알고리즘 문제다
    etc_0386 수학 문제다
    etc_0387 구현, 이분탐색 문제다
    지문 조건 해석해서 정렬된 배열로 만드는게 중요하다!
    etc_0400 수학, 소수 판정 문제다

---
+++ BaekJoon/etc/etc_0379.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. -
+이름 : 배성훈
+내용 : 피보나치 수 4
+    문제번호 : 10826번
+    
+    91부터 long 범위 초과한다
+    FFT 알고리즘이 필요하다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0379
+    {
+
+        static void Main379(string[] args)
+        {
+
+            long[] fibo = new long[10_001];
+            fibo[0] = 1;
+            fibo[1] = 1;
+            for (int i = 2; i < fibo.Length; i++)
+            {
+
+                fibo[i] = fibo[i - 2] + fibo[i - 1];
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(fibo[n]);
+        }
+    }
+}

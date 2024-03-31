7f9c7ef08c08f5e7fcf7ff9cc6711953c34be86a
TryingPop <qotjdgns2001@gmail.com>
2024-03-31 22:21:11 +0900

    feat(test) : 2024. 3 Study 백준 연습용 문제
    etc_0401 나중에 풀 문제다
    etc_0402 수학, 브루트포스, 정수론 문제다
    etc_0403 BFS, DFS 문제다
    etc_0404 수학, 정수론 문제다
    etc_0405 수학, 브루트포스 문제다
    etc_0406 두 포인터 문제다
    etc_0407 아직 안 푼 문제다
    etc_0408 dp 문제다
    etc_0409 dp 문제다
    etc_0410 백트래킹 문제다
    etc_0411 이분탐색, 누적합, 두 포인터 문제다
    etc_0412 수학, 조합론 문제다
    etc_0413 dp 문제다
    etc_0414 구현, 이분탐색 문제다

---
+++ BaekJoon/etc/etc_0401.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 31
+이름 : 배성훈
+내용 : 합과 곱
+    문제번호 : 1353번
+
+    
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0401
+    {
+
+        static void Main401(string[] args)
+        {
+
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_0407.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 31
+이름 : 배성훈
+내용 : 물통
+    문제번호 : 2251번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0407
+    {
+
+        static void Main407(string[] args)
+        {
+
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        }
+    }
+}

710d2574cf9a20e401315f4ece263e6831596a7f
TryingPop <qotjdgns2001@gmail.com>
2024-04-05 23:15:34 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0438 구현문제다
    etc_0452 수학, 구현, 브루트포스 문제다
    etc_0453 구현, 그리디, 브루트포스 문제다
    etc_0454 구현 문제다
    etc_0455 dp 문제다
    etc_0456 dp 문제다
    etc_0457 정렬, 이분탐색, 두 포인터 문제다
    etc_0458 dp 문제다
    etc_0459 아직 안 푼 문제다

---
+++ BaekJoon/etc/etc_0459.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 5
+이름 : 배성훈
+내용 : 개미의 이동
+    문제번호 : 3221번
+
+
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0459
+    {
+
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+
+        }
+    }
+}

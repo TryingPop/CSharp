ad7c11c1b252bdc2db9d7d6918bb363b8e869078
TryingPop <qotjdgns2001@gmail.com>
2024-04-12 19:49:46 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0510 dp 문제다
    etc_0511 BFS, DFS, 이분 그래프 문제다
    etc_0512 다익스트라, 최단 경로 문제다
    etc_0514 BFS 문제다
    etc_0515 그리디 문제다

---
+++ BaekJoon/etc/etc_0513.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. -
+이름 : 배성훈
+내용 : 영어와 프랑스어 (small)
+    문제번호 : 12143번
+
+    당장은 아이디어가 안떠올라 패스다
+    영어가 포함되는 경우가 있으면 해당 말의 모든 단어를 영어로 봐야한다
+    마찬가지로 프랑스어로 포함되는 경우가 있으면 해당 말의 모든 단어를 프랑스어로 봐야한다
+
+    그리고 이전의 말도 다시 확인해야한다
+    그렇게 해서 중복이 최소가 되게 세팅해야한다
+
+    그리고 처음 단어는 1000개가된다
+    해시셋으로 설정해 단어 확인을... 할까싶다
+    일단 시간 나면 나중에 풀어봐야겠다;
+    Small, Large 두 문제를 해결하려면 먼저 말들을 그룹화해서, 엮고, dp를 써야하지 않을까 싶다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0513
+    {
+
+        static void Main513(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            HashSet<string> en = new HashSet<string>(500);
+            HashSet<string> fr = new HashSet<string>(500);
+            HashSet<string> ret = new HashSet<string>(500);
+
+            string[][] words = new string[18][];
+
+            int test = ReadInt();
+            for (int t = 1; t <= test; t++)
+            {
+
+                int n = ReadInt();
+
+                AddSet(sr.ReadLine().Split(), en);
+                AddSet(sr.ReadLine().Split(), fr);
+
+                int len = n - 2;
+                for (int i = 0; i < len; i++)
+                {
+
+                    words[i] = sr.ReadLine().Split();
+
+                    int frs = 0;
+                    int ens = 0;
+                    for (int j = 0; j < words[i].Length; j++)
+                    {
+
+                        if (en.Contains(words[i][j])) ens++;
+                        if (fr.Contains(words[i][j])) frs++;
+                    }
+
+
+                }
+
+
+                en.Clear();
+                fr.Clear();
+            }
+
+            sr.Close();
+            sw.Close();
+
+            void AddSet(string[] _word, HashSet<string> _set)
+            {
+
+                for (int i = 0; i < _word.Length; i++)
+                {
+
+                    _set.Add(_word[i]);
+                }
+            }
+
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    ret = ret * 10 + c - '0';
+                }
+
+                return ret;
+            }
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_0516.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 12
+이름 : 배성훈
+내용 : 카잉 달력
+    문제번호 : 6064번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0516
+    {
+
+        static void Main516(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int test = ReadInt();
+
+            while(test-- > 0)
+            {
+
+                int m1 = ReadInt();
+                int m2 = ReadInt();
+
+                int y1 = ReadInt() - 1;
+                int y2 = ReadInt() - 1;
+
+                if (m1 < m2)
+                {
+
+                    int temp = m1;
+                    m1 = m2;
+                    m2 = temp;
+
+                    temp = y1;
+                    y1 = y2;
+                    y2 = temp;
+                }
+
+                int gcd = GetGCD(m1, m2, out int r1, out int r2);
+                if (ChkInvalidRet(y1, y2, gcd))
+                {
+
+                    sw.WriteLine(-1);
+                    continue;
+                }
+
+                int ret = y1 * m2 + y2 * m1;
+                ret %= m1 * m2 / gcd;
+                if (ret < 0) ret += (m1 * m2) / gcd;
+
+                sw.WriteLine(ret + 1);
+            }
+
+            sr.Close();
+            sw.Close();
+
+            bool ChkInvalidRet(int _y1, int _y2, int _gcd)
+            {
+
+                int diff = _y1 - _y2;
+                diff = diff < 0 ? -diff : diff;
+
+                return diff % _gcd != 0;
+            }
+
+            int GetGCD(int _a, int _b, out int _rA, out int _rB)
+            {
+
+                int r0 = _a;
+                int r1 = _b;
+                int s0 = 1;
+                int s1 = 0;
+                int t0 = 0;
+                int t1 = 1;
+                int temp = 0;
+                int q = 0;
+
+                while(r1 > 0)
+                {
+
+                    q = r0 / r1;
+
+                    temp = r0;
+                    r0 = r1;
+                    r1 = temp - r1 * q;
+
+                    temp = s0;
+                    s0 = s1;
+                    s1 = temp - s1 * q;
+
+                    temp = t0;
+                    t0 = t1;
+                    t1 = temp - t1 * q;
+                }
+
+                _rA = s0;
+                _rB = t0;
+                return r0;
+            }
+
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    ret = ret * 10 + c - '0';
+                }
+
+                return ret;
+            }
+        }
+    }
+}

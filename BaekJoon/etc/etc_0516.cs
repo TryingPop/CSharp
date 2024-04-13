aed102c5fd165219f628198632654d93befc2cfe
TryingPop <qotjdgns2001@gmail.com>
2024-04-14 00:10:56 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0516 중국인의 나머지 정리 문제다
    중국인의 나머지 정리로 답을 찾는게 힘들어
    당장은 브루트포스로 해결했다
    
    etc_0517 확장 유클리드 알고리즘 문제다
    etc_0518 확장 유클리드 알고리즘 문제다
    중국인의 나머지 정리를 풀기 위해 필요한
    확장 유클리드 알고리즘과 관련된 문제부터 풀었다
    
    etc_0519 확장 유클리드 알고리즘, 오일러 피함수 문제다
    여기까지 확장 유클리드 알고리즘 문제를 풀면서 연습을 했다
    
    etc_0520 중국인의 나머지 정리, 확장 유클리드 알고리즘 문제다
    이제 중국인의 나머지 정리를 적용하기 위해 푼 문제다
    중국인의 나머지 정리는 구현이되었으나 오버플로우와 음수 처리로
    자주 틀렸다
    
    etc_0521 문자열, 해시 문제다
    etc_0522 수학, 브루트포스 문제다
    etc_0523 다익스트라, BFS, 최단 경로 문제다
    
    어제 오늘의 대부분 문제는 연립 일차 합동식 문제이다
    정수론 때 배웠던 기억만 있을 뿐 다시 복습한다고 시간이 꽤 걸렸다

--- BaekJoon/etc/etc_0516.cs
+++ BaekJoon/etc/etc_0516.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,11 @@ using System.Threading.Tasks;
 이름 : 배성훈
 내용 : 카잉 달력
     문제번호 : 6064번
+
+    수학, 브루트포스, 정수론, 중국인의 나머지 정리 문제다
+    연립 일차 합동식의 차가 gcd를 나누지 않으면 해가 존재하지 않는다
+    그리고 중국인의 나머지 정리로 찾아보려고 했으나,
+    합성수인 경우, 막혀서 못했다;
 */
 
 namespace BaekJoon.etc
@@ -33,19 +39,7 @@ namespace BaekJoon.etc
                 int y1 = ReadInt() - 1;
                 int y2 = ReadInt() - 1;
 
-                if (m1 < m2)
-                {
-
-                    int temp = m1;
-                    m1 = m2;
-                    m2 = temp;
-
-                    temp = y1;
-                    y1 = y2;
-                    y2 = temp;
-                }
-
-                int gcd = GetGCD(m1, m2, out int r1, out int r2);
+                int gcd = GetGCD(m1, m2);
                 if (ChkInvalidRet(y1, y2, gcd))
                 {
 
@@ -53,10 +47,17 @@ namespace BaekJoon.etc
                     continue;
                 }
 
-                int ret = y1 * m2 + y2 * m1;
-                ret %= m1 * m2 / gcd;
-                if (ret < 0) ret += (m1 * m2) / gcd;
+                int ret = -1;
+                int len = m2 / gcd;
 
+                for (int i = 0; i <= len; i++)
+                {
+
+                    int calc = y1 + m1 * i;
+                    if (calc % m2 != y2) continue;
+                    ret = calc;
+                    break;
+                }
                 sw.WriteLine(ret + 1);
             }
 
@@ -72,39 +73,26 @@ namespace BaekJoon.etc
                 return diff % _gcd != 0;
             }
 
-            int GetGCD(int _a, int _b, out int _rA, out int _rB)
+            int GetGCD(int _a, int _b)
             {
 
-                int r0 = _a;
-                int r1 = _b;
-                int s0 = 1;
-                int s1 = 0;
-                int t0 = 0;
-                int t1 = 1;
-                int temp = 0;
-                int q = 0;
-
-                while(r1 > 0)
+                if (_a < _b)
                 {
 
-                    q = r0 / r1;
-
-                    temp = r0;
-                    r0 = r1;
-                    r1 = temp - r1 * q;
-
-                    temp = s0;
-                    s0 = s1;
-                    s1 = temp - s1 * q;
-
-                    temp = t0;
-                    t0 = t1;
-                    t1 = temp - t1 * q;
+                    int temp = _a;
+                    _a = _b;
+                    _b = temp;
                 }
 
-                _rA = s0;
-                _rB = t0;
-                return r0;
+                while(_b > 0)
+                {
+
+                    int temp = _a % _b;
+                    _a = _b;
+                    _b = temp;
+                }
+
+                return _a;
             }
 
             int ReadInt()
@@ -122,4 +110,118 @@ namespace BaekJoon.etc
             }
         }
     }
+#if other
+// # include <stdio.h>
+
+long long K,L;
+long long LCM;
+
+bool LinDio(int a, int b, int u){
+    LCM = a*b;
+    int Quot[200];
+    int c, ind = 0;
+    while(c = a%b){
+        Quot[ind] = a/b;
+        ind += 1; a = b; b = c;
+    }
+    
+    if(u%b!=0) return false;
+    
+    K = 0; L = 1;
+    int tmp;
+    while(ind--){
+        tmp = K;
+        K = L;
+        L = tmp - Quot[ind] * L;
+    }
+
+    K *= u/b; L *= u/b;
+    LCM /= b;
+
+    return true;
+}
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    int M,N,x,y;
+    long long ans;
+    while(T--){
+        scanf("%d %d %d %d",&M,&N,&x,&y);
+        if(LinDio(M,N,y-x)){
+            ans = M*K+x;
+            while(ans <= 0 || ans > LCM){
+                if(ans <= 0) ans += LCM;
+                if(ans > LCM) ans -= LCM;
+            }
+            printf("%u\n",ans);
+        }else{
+            printf("%d\n",-1);
+        }
+    }
+#elif other2
+import java.util.*;
+import java.io.*;
+
+// BJ #6064 - 카잉 달력
+// Strategy: 중국인의 나머지 정리, 확장 유클리드 호제법
+public class Main {
+	// EE: 확장 유클리드 호제법
+	public static long[] EE(int a, int b) {
+		if(b == 0) {
+			return new long[] {a,1,0};
+		}
+		long[] tmp = EE(b, a%b);
+		
+		return new long[] {tmp[0], tmp[2], tmp[1]-(a/b)*tmp[2]};
+	}
+	
+	// cal: 카잉달력 계산 by 중국인의 나머지 정리
+	public static long cal(int M, int N, int x, int y) {
+		long[] tmp = EE(M,N);
+		long gcd = tmp[0];
+		
+		if((y-x) % gcd != 0) {	// 베주 항등식에 의해 x-y가 gcd의 배수가 아니면 계산 불가
+			return -1;
+		}else {
+			long k0 = tmp[1];
+			long res = (M * k0 * ((y-x)/gcd) + x) % (M * N / gcd);
+			while(res <= 0) {
+				res += M * N / gcd;   // M*N/gcd -> lcm of M and N
+			}
+			return res;
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// 
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t=0; t<T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+//			if(M < N) {
+//				int tmp = M;
+//				int tmp2 = x;
+//				M = N;
+//				N = tmp;
+//				x = y;
+//				y = tmp2;
+//			}
+			
+			sb.append(cal(M,N,x,y)).append("\n");
+
+		}
+		
+		// 최종 결과 출력
+		System.out.println(sb);
+	}
+}
+#endif
 }
---
+++ BaekJoon/etc/etc_0524.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+/*
+날짜 : 2024. 4. 13
+이름 : 배성훈
+내용 : A + B
+    문제번호 : 9267번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0524
+    {
+
+        static void Main525(string[] args)
+        {
+
+            long[] input = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+
+        }
+    }
+}

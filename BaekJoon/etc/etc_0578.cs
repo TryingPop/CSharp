91f040f99d0b0e9c28b6030ab2dc517749031759
TryingPop <qotjdgns2001@gmail.com>
2024-04-20 12:29:58 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0163 게임 이론 문제다
    문제를 분석해서 짧은 시간에 정답을 도출해야한다
    
    etc_0576 이분매칭 문제다
    etc_0577 dp, 게임이론 문제다
    etc_0579 이분 매칭 문제다
    etc_0580 BFS 문제다

---
+++ BaekJoon/etc/etc_0578.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 20
+이름 : 배성훈
+내용 : 세계 일주
+    문제번호 : 31219번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0578
+    {
+
+        static void Main578(string[] args)
+        {
+
+            StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+
+
+            void Solve()
+            {
+
+
+            }
+
+
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                bool plus = true;
+                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    else if (c == '-')
+                    {
+
+                        plus = false;
+                        continue;
+                    }
+                    ret = ret * 10 + c - '0';
+                }
+
+                return plus ? ret : -ret;
+            }
+        }
+    }
+}

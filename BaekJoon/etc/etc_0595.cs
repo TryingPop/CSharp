189ceb5069fe5dbcc955e86c5251f59d65f19735
TryingPop <qotjdgns2001@gmail.com>
2024-04-22 14:04:51 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0549 dp, 이분탐색, 좌표압축 문제다
    dp를 이용한 새로운 탐색?방법인거 같다
    
    etc_0592 구현, 브루트포스 문제다
    etc_0593 트리, 분리집합 문제다
    etc_0594 문자열, 애드 혹 문제다
    etc_0596 이분매칭 문제다
    etc_0597 구현, 브루트 포스 문제다
    etc_0598 구현 문제다
    etc_0599 dp 문제다
    etc_0600 BFS 문제다

---
+++ BaekJoon/etc/etc_0595.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 22
+이름 : 배성훈
+내용 : 수학 공책
+    문제번호 : 8973번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0595
+    {
+
+        static void Main595(string[] args)
+        {
+
+            StreamReader sr;
+            int n;
+            int[] arr1;
+            int[] arr2;
+
+            void Solve()
+            {
+
+                Input();
+
+            }
+
+            void Input()
+            {
+
+                sr = new(new BufferedStream(Console.OpenStandardInput()));
+                n = ReadInt();
+                arr1 = new int[n];
+                arr2 = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    arr1[i] = ReadInt();
+                }
+
+                for (int i = n - 1; i >= 0; i--)
+                {
+
+                    arr2[i] = ReadInt();
+                }
+
+                sr.Close();
+            }
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                bool plus = true;
+                while((c= sr.Read()) != -1 && c != ' ' && c != '\n') 
+                {
+
+                    if (c == '\r') continue;
+                    else if (c == '-')
+                    {
+
+                        plus = false;
+                        continue;
+                    }
+
+                    ret = ret * 10 + c - '0';
+                }
+
+                return plus ? ret : -ret;
+            }
+        }
+    }
+}

7f4582725e30af42e7085b2025d2b31591e39500
TryingPop <qotjdgns2001@gmail.com>
2024-04-23 16:12:46 +0900

    feat(test) : 2024. 4 Study 백준 연습용 문제
    etc_0602 이분매칭 문제다

---
+++ BaekJoon/etc/etc_0601.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 22
+이름 : 배성훈
+내용 : 배열 돌리기 4
+    문제번호 : 17406번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0601
+    {
+
+        static void Main601(string[] args)
+        {
+
+            StreamReader sr;
+            StreamWriter sw;
+
+            int row, col;
+            int[][][] board;
+            int[][] rots;
+            int op;
+            Solve();
+
+            void Solve()
+            {
+
+                Input();
+            }
+
+            void Input()
+            {
+
+                sr = new(Console.OpenStandardInput(), bufferSize: 65536 * 8);
+                row = ReadInt();
+                col = ReadInt();
+
+                op = ReadInt();
+                board = new int[3][][];
+
+                // 2번이 회전 시킨 결과
+                // 1번은 연산용
+                // 0번은 초기 상태확인용도!
+                board[0] = new int[row][];
+                board[1] = new int[row][];
+                board[2] = new int[row][];
+
+                for (int r = 0; r < row; r++)
+                {
+
+                    board[0][r] = new int[col];
+                    board[1][r] = new int[col];
+                    board[2][r] = new int[col];
+
+                    for (int c = 0; c < col; c++)
+                    {
+
+                        board[0][r][c] = ReadInt();
+                    }
+                }
+
+                rots = new int[op][];
+                for (int i = 0; i < op; i++)
+                {
+
+                    rots[i] = new int[3];
+                    for (int j = 0; j < 3; j++)
+                    {
+
+                        rots[i][j] = ReadInt();
+                    }
+                }
+
+                sr.Close();
+            }
+
+            void Copy(int _f, int _t)
+            {
+
+                for (int i = 0; i < row; i++)
+                {
+
+                    for (int j = 0; j < col; j++)
+                    {
+
+                        board[_t][i][j] = board[_f][i][j];
+                    }
+                }
+            }
+
+            void Rotate(int _rotIdx)
+            {
+
+                // board[1]에 값으로 -> board[2]로 연산
+                for (int size = rots[_rotIdx][2]; size >= 2; size--)
+                {
+
+
+                }
+            }
+
+            int GetMin()
+            {
+
+                int ret = 0;
+                for (int c = 0; c < col; c++)
+                {
+
+                    ret += board[2][0][c];
+                }
+
+                for (int r = 1; r < row; r++)
+                {
+
+                    int calc = 0;
+                    for (int c = 0; c < col; c++)
+                    {
+
+                        calc += board[2][r][c];
+                    }
+
+                    ret = calc < ret ? calc : ret;
+                }
+
+                return ret;
+            }
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    ret = ret * 10 + c - '0';
+                }
+
+                return ret;
+            }
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_0602.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 4. 23
+이름 : 배성훈
+내용 : 사랑의 큐피드
+    문제번호 : 30976번
+
+    이분 매칭 문제다
+    경우의 수가 300, 300이므로, 완전탐색 300 * 300 = 90_000으로
+    간선을 놓고 이분 매칭을 진행해서 풀었다
+    초과, 미만 부분을 이상, 이하로 해서 3번 틀렸다
+    그리고 인덱스 에러로 1번 틀렸다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0602
+    {
+
+        static void Main602(string[] args)
+        {
+
+            StreamReader sr;
+
+            int n, m;
+            int[] match;
+            bool[] visit;
+
+            List<int>[] line;
+
+            Solve();
+
+            void Solve()
+            {
+
+                Input();
+
+                int ret = 0;
+                for (int i = 1; i <= n; i++)
+                {
+
+                    Array.Fill(visit, false);
+                    if (DFS(i)) ret++;
+                }
+
+                Console.WriteLine(ret);
+            }
+
+            void Input()
+            {
+
+                sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+                n = ReadInt();
+                m = ReadInt();
+                int[] girl = new int[n + 1];
+                line = new List<int>[n + 1];
+                for (int i = 1; i <= n; i++)
+                {
+
+                    girl[i] = ReadInt();
+                    line[i] = new();
+                }
+
+                int[] boy = new int[m + 1];
+                for (int i = 1; i <= m; i++)
+                {
+
+                    boy[i] = ReadInt();
+                }
+
+                int[] want = new int[n + 1];
+                for (int i = 1; i <= n; i++)
+                {
+                    want[i] = ReadInt();
+                }
+
+                for (int b = 1; b <= m; b++)
+                {
+
+                    int cur = ReadInt();
+
+                    for (int g = 1; g <= n; g++)
+                    {
+
+                        if (boy[b] >= want[g] || girl[g] <= cur) continue;
+                        line[g].Add(b);
+                    }
+                }
+
+                match = new int[m + 1];
+                visit = new bool[m + 1];
+
+                sr.Close();
+            }
+
+            bool DFS(int _n)
+            {
+
+                for (int i = 0; i < line[_n].Count; i++)
+                {
+
+                    int next = line[_n][i];
+                    if (visit[next]) continue;
+                    visit[next] = true;
+
+                    if (match[next] == 0 || DFS(match[next]))
+                    {
+
+                        match[next] = _n;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            int ReadInt()
+            {
+
+                int c, ret = 0;
+                while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                {
+
+                    if (c == '\r') continue;
+                    ret = ret * 10 + c - '0';
+                }
+
+                return ret;
+            }
+        }
+    }
+}

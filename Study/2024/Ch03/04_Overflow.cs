15d9ea980df6d3ca057bbb38bdda73639624df7e
TryingPop <qotjdgns2001@gmail.com>
2024-10-22 22:23:29 +0900

    feat: 문법 공부 2024. 10 Study
    교재 p 54 ~ 59
    오버플로우와 부동소수점에 관련된 내용이 있다

---
+++ Study/2024/Ch03/04_Overflow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 10. 22
+이름 : 배성훈
+내용 : Overflow
+    교재 p55 ~ 56
+    
+    오버플로우에 관한 내용이다
+    최저값보다 작은 데이터는 언더플로우로 명시했다
+*/
+
+namespace Study._2024.Ch03
+{
+    internal class _04_Overflow
+    {
+
+        static void Main4(string[] args)
+        {
+
+            // uint a = uint.MaxValue;
+            int a = int.MaxValue;
+            Console.WriteLine(a);       // 주석 : 4_294_967_295
+                                        // 2_147_483_647
+
+            a = a + 1;                  
+
+            Console.WriteLine(a);       // 주석 : 0
+                                        // -2_147_483_648
+        }
+    }
+}
---
+++ Study/2024/Ch03/05_FloatingPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 10. 22
+이름 : 배성훈
+내용 : FloatingPoint
+    교재 p 56 ~ 59
+
+    부동 소숫점에 관련된 내용이 있다
+    float은 7개의 자릿수만 다룰 수 있다 한다
+    double은 15 ~ 16개의 자릿수를 다룰 수 있다 한다
+
+    float는 1비트를 부호 전용, 가수부 23비트를 수를 표현하는데 사용한다
+    그리고 나머지 지수부 8비트를 소수점의 위치를 나타내기 위해 사용한다고 한다
+
+    float은 넓은 범위를 수를 다룰 수 있다
+    +-3.402823e38 (e38은 10^38)
+
+    실수 자료형은 데이터 손실이 최소화되어야 해서
+    float보다는 double이 좋다 더 정밀하게 해야한다면 decimal를 이용해야한다
+*/
+
+namespace Study._2024.Ch03
+{
+    internal class _05_FloatingPoint
+    {
+
+        static void Main5(string[] args)
+        {
+
+            float a = 3.1415_9265_3589_7932_3846f;
+            Console.WriteLine(a);   // 3.1415927
+
+            double b = 3.1415_9265_3589_7932_3846;
+            Console.WriteLine(b);   // 3.141_592_653_589_793
+        }
+    }
+}

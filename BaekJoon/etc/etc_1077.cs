d5e41648f5d1f9f75624e20369fd1b7e5b3b8829
TryingPop <qotjdgns2001@gmail.com>
2024-10-24 23:50:39 +0900

    feat: 백준 연습용 문제 2024. 10 Study
    etc_1075 그리디 문제다
    가장 등수 숫자 값이 낮은 경우에 조심해야한다
    규칙성을 찾아 풀었다
    
    etc_1077 그리디 문제다
    아이디어가 떠오르지 않아 해설을 봤고,
    구현을 잘못해 4번이상 틀렸다
    아이디어는 다음과 같다
    정렬한 뒤 두 포인터로 n개 문제가 포함되었는지 확인하며
    최솟값을 찾는다

--- BaekJoon/etc/etc_1077.cs
+++ BaekJoon/etc/etc_1077.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 이름 : 배성훈
 내용 : 대회 개최
     문제번호 : 31411번
+
+    그리디, 정렬, 두 포인터 문제다
+    
 */
 
 namespace BaekJoon.etc
@@ -16,6 +19,137 @@ namespace BaekJoon.etc
     internal class etc_1077
     {
 
+        static void Main1077(string[] args)
+        {
 
+            StreamReader sr;
+            int n, k;
+            (int val, int d)[] arr;
+
+            Solve();
+            void Solve()
+            {
+
+                Input();
+
+                GetRet();
+            }
+
+            void GetRet()
+            {
+
+                Array.Sort(arr, (x, y) => x.val.CompareTo(y.val));
+                int l = 0;
+                int r = -1;
+                int[] cnt = new int[n];
+                int match = 0;
+
+                int len = n * k;
+                int ret = 200_000;
+                while (r < len)
+                {
+
+                    if (match < n)
+                    {
+
+                        if (r == len - 1) break;
+                        r++;
+                        int chk = cnt[arr[r].d]++;
+                        if (chk == 0) match++;
+                    }
+                    else
+                    {
+
+                        ret = Math.Min(ret, arr[r].val - arr[l].val);
+                        int chk = cnt[arr[l].d]--;
+                        if (chk == 1) match--;
+                        l++;
+                    }
+                }
+
+                Console.Write(ret);
+            }
+
+            void Input()
+            {
+
+                sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                n = ReadInt();
+                k = ReadInt();
+
+                arr = new (int val, int d)[n * k];
+                int idx = 0;
+                for (int r = 0; r < n; r++)
+                {
+
+                    for (int c = 0; c < k; c++)
+                    {
+
+                        arr[idx++] = (ReadInt(), r);
+                    }
+                }
+
+                sr.Close();
+
+                int ReadInt()
+                {
+
+                    int ret = 0;
+                    while (TryReadInt()) { }
+                    return ret;
+
+                    bool TryReadInt()
+                    {
+
+                        int c = sr.Read();
+                        if (c == '\r') c = sr.Read();
+                        if (c == '\n' || c == ' ') return true;
+                        ret = c - '0';
+
+                        while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                        {
+
+                            if (c == '\r') continue;
+                            ret = ret * 10 + c - '0';
+                        }
+
+                        return false;
+                    }
+                }
+            }
+        }
     }
+#if other
+// #include <bits/stdc++.h>
+// #include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int N, K; cin >> N >> K;
+    vector<int> v, vis(N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
+            int x; cin >> x;
+            v.push_back(x * N + i);
+        }
+    }
+    sort(v.begin(), v.end());
+    
+    int cnt = 0, ans = 1e9 + 7;
+    int l = 0, r = 0;
+    while (r < N * K) {
+        while (r < N * K && cnt < N) {
+            int xx = v[r] / N, ii = v[r] % N;
+            if (!vis[ii]++) cnt++;
+            r++;
+        }
+        if (cnt >= N) ans = min(ans, v[r - 1] / N - v[l] / N);
+        cnt -= !--vis[v[l++] % N];
+    }
+
+    cout << ans;
+}
+
+#endif
 }

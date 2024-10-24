d146f04130cbaa14d72472dc5b1a02d7a6c07521
TryingPop <qotjdgns2001@gmail.com>
2024-10-24 15:13:45 +0900

    feat: 백준 연습용 문제 2024. 10 Study
    etc_1074 피보나치 홀수항 합 문제다
    F(1) = F(2)라는 사실을 이용하면
    F(1) + F(3) + ... + F(2n - 1) = F(2n)이 됨을 알 수 있다
    
    etc_1076 약수 개수 맞추는 문제다
    처음에는 모두 찾아 priority queue로 작은수부터 곱해줬다
    그런데 그냥 배열에 저장한 뒤 순차적으로 읽으면 될거 같아 수정하니
    84ms -> 68ms로 통과한다

---
+++ BaekJoon/etc/etc_1077.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 10. 24
+이름 : 배성훈
+내용 : 대회 개최
+    문제번호 : 31411번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1077
+    {
+
+
+    }
+}

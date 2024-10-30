dc8f6bd35c750c9822070b1652f8fbdd61b42ac9
TryingPop <qotjdgns2001@gmail.com>
2024-10-30 19:23:18 +0900

    feat: 클래스 2024. 10 문법 공부
    객체지향 프로그래밍은 코드내의 모든 것을 객체로 표현하려는 것이다
    객체는 속성과 메소드를 포함한 것이다
    
    그리고 C#에서는 클래스를 이용해 객체를 표현한다
    이후 객체의 선언 방법과 생성자와 소멸자에 대한 설명이 있다

---
+++ Study/2024/Ch07_클래스/코드/01_BasicClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 10. 30
+이름 : 배성훈
+내용 : 객체지향 프로그래밍과 클래스
+    교재 p 221 ~ 226
+
+    객체지향 프로그래밍과 객체에 대한 설명이 있다
+    그리고 클래스와 클래스에 대한 설명이 있다
+
+    
+*/
+
+namespace Study._2024.Ch07_클래스.코드
+{
+    internal class _01_BasicClass
+    {
+
+        class Cat
+        {
+
+            public string Name;
+            public string Color;
+
+            public void Meow()
+            {
+
+                Console.WriteLine($"{Name} : 야옹");
+            }
+        }
+
+        static void Main1(string[] args)
+        {
+
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+
+            kitty.Meow();                                       // 키티 : 야옹
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}"); // 키티 : 하얀색
+
+            Cat nero = new Cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+
+            nero.Meow();                                        // 네로 : 야옹
+            Console.WriteLine($"{nero.Name} : {nero.Color}");   // 네로 : 검은색
+        }
+    }
+}
---
+++ Study/2024/Ch07_클래스/코드/02_Constructor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 10. 30
+이름 : 배성훈
+내용 : 객체의 삶과 죽음 : 생성자와 종료자
+    교재 p 226 ~ 231
+
+    생성자와 종료자에 대해 설명하고 있다
+    종료자는 가비지 컬렉터에 맡기는게 좋다고 해서 정의안하는게 좋다고 한다
+*/
+
+namespace Study._2024.Ch07_클래스.코드
+{
+    internal class _02_Constructor
+    {
+
+        class Cat
+        {
+
+            public Cat()
+            {
+
+                Name = "";
+                Color = "";
+            }
+
+            public Cat(string _Name, string _Color)
+            {
+
+                Name = _Name;
+                Color = _Color;
+            }
+
+            ~Cat()
+            {
+
+                Console.WriteLine($"{Name}: 잘가");
+            }
+
+            public string Name;
+            public string Color;
+
+            public void Meow()
+            {
+
+                Console.WriteLine($"{Name}: 야옹");
+            }
+        }
+
+        static void Main2(string[] args)
+        {
+
+            Cat kitty = new Cat("키티", "하얀색");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name}: {kitty.Color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name}: {nero.Color}");
+        }
+    }
+}

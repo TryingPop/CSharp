3efdfa3facc7834e3b7e46d303a85a72e5239d11
TryingPop <qotjdgns2001@gmail.com>
2024-11-02 11:05:57 +0900

    fix: 입출력함수 수정 2024. 11 Study
    etc_1092는 다음 etc 번호를 위해 만든 빈코드다
    
    etc_0397 테스트 케이스가 추가되면서,
    문자열 끝에 공백이 끼인 케이스가 추가되었다
    논리는 맞게 보였고 채점현황에 다른사람 기록들을 보니
    Format 에러가 잔뜩 있었다.
    Format 에러는 문자열 변환 에러인데, 불필요한 공백으로 자주 나는 에러다

--- BaekJoon/etc/etc_0397.cs
+++ BaekJoon/etc/etc_0397.cs
@@ -15,6 +15,11 @@ using System.Threading.Tasks;
     입력 숫자 범위는 100만이고, 개수는 100개까지 들어온다
     그래서 100만 * 5000개이므로, int 범위를 초월한다
     이후에는 GCD를 모두 합치니 이상없이 통과했다
+
+    2024. 11. 2 오늘 틀린 문제 기록을 보니 추가되어 있어 수정했다.
+    이전 코드를 보니 논리엔 이상없어 보이고, 다른 사람 채점현황 보니 
+    Format 에러가 있었다. Format 에러는 문자열 변환 에러이므로 입력 문제라 판단하고
+    입력 부분만 수정하니 이상없이 통과했다.
 */
 
 namespace BaekJoon.etc
@@ -64,14 +69,6 @@ namespace BaekJoon.etc
             int GetGcd(int _a, int _b)
             {
 
-                if (_a < _b)
-                {
-
-                    int temp = _a;
-                    _a = _b;
-                    _b = temp;
-                }
-
                 while(_b > 0)
                 {
 
@@ -86,15 +83,27 @@ namespace BaekJoon.etc
             int ReadInt()
             {
 
-                int c, ret = 0;
-                while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                int ret = 0;
+                while (TryReadInt()) { }
+                return ret;
+
+                bool TryReadInt()
                 {
 
-                    if (c == '\r') continue;
-                    ret = ret * 10 + c - '0';
-                }
+                    int c = sr.Read();
+                    if (c == '\r') c = sr.Read();
+                    if (c == ' ' || c == '\n') return true;
 
-                return ret;
+                    ret = c - '0';
+                    while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                    {
+
+                        if (c == '\r') continue;
+                        ret = ret * 10 + c - '0';
+                    }
+
+                    return false;
+                }
             }
         }
     }

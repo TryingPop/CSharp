2fc16c6fded5f8bf8bc4ae1be2e45ce2e15c8c54
TryingPop <qotjdgns2001@gmail.com>
2024-11-08 21:48:13 +0900

    feat : 람다식 2024. 11 문법 공부 14장
    람다식과 람다문 내용이 있다

---
+++ Study/2024/Ch14_람다식/코드/01_SimpleLambda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 11. 8
+이름 : 배성훈
+내용 : 람다식
+    교재 p 487 ~ 490
+
+    람다식은 영어로 Lambda Expression이라 하고,
+    λ - Expression이라고 표기하기도 한다.
+
+    분명하고 간결한 방법으로 함수를 묘사하기 위해 람다 계산법을 고안했다.
+    람다 계산법에서는 모든 것이 함수로 이루어져 있다.
+
+    람다 계산법에서 어떤 값을 변수에 대입하고 싶으면 함수를 변수에 대입하며, 
+    이것을 함수의 적용이라 불렀다.
+    그리고 이를 컴퓨터에 도입한게 람다식이다.
+    C# 뿐만 아니라 C++, 자바, 파이썬에도 람다식을 지원한다.
+
+    람다식은 익명 메소드를 만들기 위해 사용한다.
+    람다식으로 만드는 익명 메소드는 무명 함수(Anonymous Function)라는 이름으로 부른다.
+    람다식도 매개변수와 반환 값을 갖고 있다.
+
+    C# 컴파일러는 형식 유추(Type Inference)라는 기능을 제공해 변수만 넣어도 형식을 유추해준다.
+    그래서 간결하게 작성 가능하다.
+*/
+
+namespace Study._2024.Ch14_람다식.코드
+{
+    internal class _01_SimpleLambda
+    {
+
+        delegate int Calculate(int a, int b);
+
+        static void Main1(string[] args)
+        {
+
+            Calculate calc = (a, b) => a + b;
+
+            Console.WriteLine($"{3} + {4} : {calc(3, 4)}");
+        }
+    }
+}
---
+++ Study/2024/Ch14_람다식/코드/02_StatementLambda.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 11. 8
+이름 : 배성훈
+내용 : 문 형식의 람다식
+    교재 p 490 ~ 492
+
+    람다식은 식(Expression) 형식을 하고 있다.
+    문 형식의 람다 식(Statement Lambda)는 => 연산자의 오른편에 
+    식 대신 { }로 둘러싸인 코드 블록이 위치한다.
+*/
+
+namespace Study._2024.Ch14_람다식.코드
+{
+    internal class _02_StatementLambda
+    {
+
+        delegate string Concatenate(string[] args);
+
+        static void Main2(string[] args)
+        {
+
+            Concatenate concat =
+                (arr) =>
+                {
+
+                    string result = "";
+                    foreach (string s in arr)
+                        result += s;
+
+                    return result;
+                };
+
+            Console.WriteLine(concat(args));
+        }
+    }
+}
